aa45325da15c12543ca538c1aaf7af31f2eb9e63
Tiffany Kalin <tkalin@untangle.com>
2021-03-25 16:33:38 -0600

    waf-61: wip: moving to v3 license

--- services/settings/license.go
+++ services/settings/license.go
@@ -1,11 +1,8 @@
 package settings
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"time"
 
@@ -17,20 +14,21 @@ type LicenseSub struct {
 	enabledServices map[string]bool
 	licenseFilename string
 	uidFile         string
+	product         string
 }
 
 // License is the struct representing each individual license
 type License struct {
-	UID         string `json:UID`
-	Type        string `json:type`
-	End         int    `json:end`
-	Start       int    `json:start`
-	Seats       int    `json:seats default:-1`
+	UID         string `json:"UID"`
+	Type        string `json:"type"`
+	End         int    `json:"end"`
+	Start       int    `json:"start"`
+	Seats       int    `json:"seats" default:"-1"`
 	DisplayName string `json:"displayName"`
-	Key         string `json:key`
-	KeyVersion  int    `json:keyVersion`
-	Name        string `json:name`
-	javaClass   string `json:javaClass`
+	Key         string `json:"key"`
+	KeyVersion  int    `json:"keyVersion"`
+	Name        string `json:"name"`
+	JavaClass   string `json:"javaClass"`
 }
 
 const (
@@ -38,27 +36,33 @@ const (
 )
 
 // NewLicenseSub creates new license
-func NewLicenseSub(licenseFilename string, uidFile string) *LicenseSub {
+func NewLicenseSub(licenseFilename string, uidFile string, product string) *LicenseSub {
 	logger.Info("Starting license sub...\n")
 
 	l := new(LicenseSub)
 	l.enabledServices = l.GetDefaults()
 	l.licenseFilename = licenseFilename
 	l.uidFile = uidFile
+	l.product = product
 
 	return l
 }
 
 // GetDefaults gets the default enabled services file, where everything is disabled
-// TODO: flag for product
 func (l *LicenseSub) GetDefaults() map[string]bool {
-	return map[string]bool{
-		"loadBalancing":    false,
-		"sslCertUpload":    false,
-		"advancedLogging":  false,
-		"manualRuleConfig": false,
-		"ruleException":    false,
+	var defaults map[string]bool
+	switch l.product {
+	case "WAF":
+		defaults = map[string]bool{
+			"loadBalancing":    false,
+			"sslCertUpload":    false,
+			"advancedLogging":  false,
+			"manualRuleConfig": false,
+			"ruleException":    false,
+		}
 	}
+
+	return defaults
 }
 
 // CleanUp cleans up the contexts of the licenseSub
@@ -90,79 +94,13 @@ func (l *LicenseSub) GetLicenses() (map[string]bool, error) {
 	}
 
 	// unmarshal license inforamtion
-	var licenses []License
+	var licenses map[string]bool
 	jsonErr := json.Unmarshal(fileBytes, &licenses)
 	if jsonErr != nil {
 		l.enabledServices = l.GetDefaults()
 		return l.enabledServices, jsonErr
 	}
 
-	// determine if valid information
-	for _, license := range licenses {
-		isValid := l.isLicenseValid(license)
-		if isValid {
-			l.enabledServices[license.Name] = true
-		}
-	}
-
+	l.enabledServices = licenses
 	return l.enabledServices, nil
-}
-
-// isLicenseValid checks if a given license is valid
-func (l *LicenseSub) isLicenseValid(license License) bool {
-	timeNow := time.Now()
-
-	// Check if a valid type of service for the license
-	_, ok := l.enabledServices[license.Name]
-	if !ok {
-		logger.Warn("Invalid license " + license.Name + ": Could not find in enabledServices map")
-		return false
-	}
-
-	// start date in future
-	if license.Start > int(timeNow.Unix()) {
-		logger.Warn("Invalid license " + license.Name + ": Invalid start date (After today)\n")
-		return false
-	}
-
-	// expired
-	if license.End < int(timeNow.Unix()) {
-		logger.Warn("Invalid license " + license.Name + ": Expired\n")
-		return false
-	}
-
-	// check the uid
-	uid, uidErr := GetUID(l.uidFile)
-	if uidErr == nil {
-		if len(license.UID) == 0 || license.UID != uid {
-			logger.Warn("Invalid license " + license.Name + ": Invalid (UID mistmatch) \n")
-			return false
-		}
-	} else {
-		logger.Warn("Can't get uid check for license\n")
-		return false
-	}
-
-	// get key
-	var input string
-	salt := "the meaning of life is 42"
-	if license.KeyVersion == 1 || license.Seats == -1 {
-		input = fmt.Sprintf("%d%s%s%s%d%d%s", license.KeyVersion, license.UID, license.Name, license.Type, license.Start, license.End, salt)
-	} else if license.KeyVersion == 3 {
-		input = fmt.Sprintf("%d%s%s%s%d%d%d%s", license.KeyVersion, license.UID, license.Name, license.Type, license.Start, license.End, license.Seats, salt)
-	} else {
-		logger.Warn("Invalid license " + license.Name + ": Invalid key version\n")
-		return false
-	}
-
-	hasher := md5.New()
-	hasher.Write([]byte(input))
-	hex := hex.EncodeToString(hasher.Sum(nil))
-
-	if hex != license.Key {
-		logger.Warn("Invalid license " + license.Name + ": Invalid (key mistmatch) \n")
-		return false
-	}
-
-	return true
 }
