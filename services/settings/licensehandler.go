19b55b31a833cefd7f2e23dd6d549927b91c77e0
Christian Rasmussen <72759223+rasmussen-untangle@users.noreply.github.com>
2021-04-29 16:00:33 -0600

    MFW-1406 (#12)

--- services/settings/licensehandler.go
+++ services/settings/licensehandler.go
@@ -31,9 +31,9 @@ type LicenseInfo struct {
 type License struct {
 	UID         string `json:"UID"`
 	Type        string `json:"type"`
-	End         int    `json:"end"`
-	Start       int    `json:"start"`
-	Seats       int    `json:"seats" default:"-1"`
+	End         int64  `json:"end"`
+	Start       int64  `json:"start"`
+	Seats       int64  `json:"seats" default:"-1"`
 	DisplayName string `json:"displayName"`
 	Key         string `json:"key"`
 	KeyVersion  int    `json:"keyVersion"`
