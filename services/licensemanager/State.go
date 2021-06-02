874c66c140b9c52e2ce916a58dc2f1d2021793b9
John Sommerville <jsommerville@untangle.com>
2021-06-02 14:57:39 -0600

    add fromString helper to State

--- services/licensemanager/State.go
+++ services/licensemanager/State.go
@@ -1,5 +1,10 @@
 package licensemanager
 
+import (
+	"errors"
+	"strings"
+)
+
 // State is an enum used for indicating service state
 type State int
 
@@ -8,3 +13,16 @@ const (
 	StateEnable State = iota
 	StateDisable
 )
+
+func (state *State) fromString(cmd string) (State, error) {
+
+	switch lowerCmd := strings.ToLower(cmd); lowerCmd {
+	case "enable":
+		return StateEnable, nil
+	case "disable":
+		return StateDisable, nil
+	default:
+		return StateDisable, errors.New("Unable to parse state\n")
+
+	}
+}
