f3f7b536b2e471bdcb3b540e5f9cf1934e265c98
Jacob Tschirhart <jtschirhart@untangle.com>
2021-10-27 09:52:39 -0600

    mfw-1185 check for file name in cpu profiler

--- services/profiler/cpuProfiler.go
+++ services/profiler/cpuProfiler.go
@@ -1,6 +1,7 @@
 package profiler
 
 import (
+	"errors"
 	"os"
 	"runtime/pprof"
 
@@ -14,6 +15,9 @@ type CPUProfiler struct {
 
 // StartCPUProfile sets up and starts cpu profiling
 func (cpuProfiler *CPUProfiler) StartCPUProfile() error {
+	if cpuProfiler.CPUProfileFileName == "" {
+		return errors.New("Cannot start cpu profiling. CPUProfileFileName must be specified!\n")
+	}
 	cpu, err := os.Create(cpuProfiler.CPUProfileFileName)
 	if err != nil {
 		logger.Alert("+++++ Error creating file for CPU profile: %v ++++++\n", err)
