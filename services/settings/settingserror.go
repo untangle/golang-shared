407954d7b4d080aace1a86a6353698dc3cacbf41
Tiffany Kalin <tkalin@untangle.com>
2021-11-30 14:34:01 -0700

    mfw-1711: linting for new structs

--- services/settings/settingserror.go
+++ services/settings/settingserror.go
@@ -1,14 +1,17 @@
 package settings
 
+// SetSettingsError TODO
 type SetSettingsError struct {
 	Confirm Confirmation `json:"CONFIRM"`
 }
 
+// Confirmation TODO
 type Confirmation struct {
 	Rules    map[string]InvalidType `json:"RULES"`
 	Policies map[string]InvalidType `json:"POLICIES"`
 }
 
+// InvalidType TODO
 type InvalidType struct {
 	AffectedValue      string `json:"affectedValue"`
 	InvalidReason      string `json:"invalidReason"`
@@ -17,12 +20,14 @@ type InvalidType struct {
 	Parent             string `json:"parent"`
 }
 
+// SetSettingsErrorUI TODO
 type SetSettingsErrorUI struct {
 	MainTranslationString string          `json:"mainTranslationString"`
 	InvalidReason         string          `json:"invalidReason"`
 	AffectedValues        []AffectedValue `json:"affectedValues"`
 }
 
+// AffectedValue TODO
 type AffectedValue struct {
 	AffectedType  string `json:"affectedType"`
 	AffectedValue string `json:"affectedValue"`
