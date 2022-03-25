cb527704df2ac3a11d790e5593eb25d1316b4736
dhadarau <dhadarau@arista.com>
2022-03-25 18:30:12 +0200

    MFW-1882 Use struct to parse LLDP json output

--- plugins/lldp/lldp.go
+++ plugins/lldp/lldp.go
@@ -3,12 +3,64 @@ package lldp
 import (
 	"encoding/json"
 	"os/exec"
-	"reflect"
 
 	"github.com/untangle/discoverd/services/discovery"
 	"github.com/untangle/golang-shared/services/logger"
 	"github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
 )
+
+type Json struct {
+	Lldp []Lldp `json:"lldp"`
+}
+
+type Lldp struct {
+	Intf []Intf `json:"interface"`
+}
+
+type Intf struct {
+	Chassis []Chassis `json:"chassis"`
+	LldpMed []LldpMed `json:"lldp-med"`
+}
+
+type Chassis struct {
+	Id []ChassisId                 `json:"id"`
+	Name []Value                   `json:"name"`
+	Desc []Value                   `json:"descr"`
+	Capability []ChassisCapability `json:"capability"`
+}
+
+type ChassisId struct {
+	Type  string `json:"type,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type ChassisCapability struct {
+	Type string  `json:"type,omitempty"`
+	Enabled bool `json:"enabled,omitempty"`
+}
+
+type LldpMed struct {
+	DeviceType []Value             `json:"device-type"`
+	Capability []LldpMedCapability `json:"capability"`
+	Inventory []LldpMedInventory   `json:"inventory"`
+}
+
+type LldpMedCapability struct {
+	Type string    `json:"type,omitempty"`
+	Available bool `json:"available,omitempty"`
+}
+
+type LldpMedInventory struct {
+	Hardware []Value     `json:"hardware"`
+	Software []Value     `json:"software"`
+	Serial []Value       `json:"serial"`
+	Manufacturer []Value `json:"manufacturer"`
+	Model []Value        `json:"model"`
+}
+
+type Value struct {
+	Value string `json:"value,omitempty"`
+}
 
 // Start starts the LLDP collector
 func Start() {
@@ -28,41 +80,25 @@ func LldpcallBackHandler(commands []discovery.Command) {
 	logger.Debug("LLDP neighbors callback handler: Received %d commands\n", len(commands))
 
 	// run neighbors command
-	cmd := exec.Command("lldpcli", "-f", "json", "show", "n", "details")
+	cmd := exec.Command("lldpcli", "-f", "json0", "show", "n", "details")
 	output, _ := cmd.CombinedOutput()
 
 	// logger.Info("LLDP output: %s\n", string(output))
 
 	// parse json output data
-	var result map[string]interface{}
+	var result Json
 	if err := json.Unmarshal([]byte(output), &result); err != nil {
 		logger.Err("Unable to unmarshal json: %s\n", err)
 	}
 
-	lldp, _ := result["lldp"].(map[string]interface{})
-
-	if (lldp["interface"] == nil) {
+	// return on empty data
+	if (len(result.Lldp[0].Intf) == 0) {
 		logger.Debug("No LLDP neighbors found!\n")
 		return
 	}
 
-	interfacesType := reflect.TypeOf(lldp["interface"])
-
-	// create a slice (array) of interfaces
-	interfaces := make([]interface{}, 0, 0)
-	// when having an array of interfaces
-	if (interfacesType.Kind() == reflect.Slice) {
-		interfaces = lldp["interface"].([]interface{})
-	}
-	// when just a single interface
-	if (interfacesType.Kind() == reflect.Map) {
-		for _, value := range lldp["interface"].(map[string]interface {}) {
-			interfaces = append(interfaces, value)
-		}
-	}
-
 	// iterate over interface items
-	for _, intf := range interfaces {
+	for _, intf := range result.Lldp[0].Intf {
 		// initialize the discovery entry
 		entry := discovery.DeviceEntry{}
 		entry.Init()
@@ -71,49 +107,64 @@ func LldpcallBackHandler(commands []discovery.Command) {
 		// mac is used as id for discovery entry update
 		var mac = ""
 
-		// processing interfaces "eth0": { ... }
-		for _, value := range intf.(map[string]interface {}) {
-			// chassis
-			chassis, _ := value.(map[string]interface {})["chassis"]
-			if (chassis == nil) {
-				continue
-			}
-			for key, value := range chassis.(map[string]interface {}) {
-				entry.Data.Lldp.SysName = key
-				ch, _ := value.(map[string]interface {})
-				entry.Data.Lldp.SysDesc = ch["descr"].(string)
-
-				chassis_id, _ := ch["id"].(map[string]interface{})
-				chassis_id_type, _ := chassis_id["type"].(string)
-				chassis_id_value, _ := chassis_id["value"].(string)
-
-				// if chassis id type is "mac", populate mac value
-				if (chassis_id_type == "mac") {
-					mac = chassis_id_value
+		if (len(intf.Chassis) > 0) {
+			chassis := intf.Chassis[0]
+			// mac discovery
+			if (len(chassis.Id) > 0) {
+				if (chassis.Id[0].Type == "mac") {
+					mac = chassis.Id[0].Value
 				}
 			}
 
-			// LLDP-MED
-			lldp_med, _ := value.(map[string]interface{})["lldp-med"]
-			if (lldp_med == nil) {
-				continue
+			if (len(chassis.Name) > 0) {
+				entry.Data.Lldp.SysName = chassis.Name[0].Value
 			}
-			inventory, _ := lldp_med.(map[string]interface {})["inventory"]
-			if (inventory == nil) {
-				continue
+			if (len(chassis.Desc) > 0) {
+				entry.Data.Lldp.SysDesc = chassis.Desc[0].Value
 			}
-			for inv_key, inv_map := range inventory.(map[string]interface{}) {
-				switch inv_key {
-					case "hardware":
-						entry.Data.Lldp.InventoryHWRev = inv_map.(string)
-					case "software":
-						entry.Data.Lldp.InventorySoftRev = inv_map.(string)
-					case "serial":
-						entry.Data.Lldp.InventorySerial = inv_map.(string)
-					case "model":
-						entry.Data.Lldp.InventoryModel = inv_map.(string)
-					case "manufacturer":
-						entry.Data.Lldp.InventoryVendor = inv_map.(string)
+
+			// chasis capabilities
+			if (len(chassis.Capability) > 0) {
+				for _, val := range chassis.Capability {
+					cap := &Discoverd.LLDPCapabilities{}
+					cap.Capability = val.Type
+					cap.Enabled = val.Enabled
+					entry.Data.Lldp.ChassisCapabilities = append(entry.Data.Lldp.ChassisCapabilities, cap)
+				}
+			}
+		}
+
+		if (len(intf.LldpMed) > 0) {
+			lldpmed := intf.LldpMed[0]
+
+			// LLDP-MED inventory
+			if (len(lldpmed.Inventory) > 0) {
+				inv := lldpmed.Inventory[0]
+
+				if (len(inv.Hardware) > 0) {
+					entry.Data.Lldp.InventoryHWRev = inv.Hardware[0].Value
+				}
+				if (len(inv.Software) > 0) {
+					entry.Data.Lldp.InventorySoftRev = inv.Software[0].Value
+				}
+				if (len(inv.Serial) > 0) {
+					entry.Data.Lldp.InventorySerial = inv.Serial[0].Value
+				}
+				if (len(inv.Model) > 0) {
+					entry.Data.Lldp.InventoryModel = inv.Model[0].Value
+				}
+				if (len(inv.Manufacturer) > 0) {
+					entry.Data.Lldp.InventoryVendor = inv.Manufacturer[0].Value
+				}
+			}
+
+			// LLDP-MED capabilities
+			if (len(lldpmed.Capability) > 0) {
+				for _, val := range lldpmed.Capability {
+					cap := &Discoverd.LLDPCapabilities{}
+					cap.Capability = val.Type
+					cap.Enabled = val.Available
+					entry.Data.Lldp.MedCapabilities = append(entry.Data.Lldp.MedCapabilities, cap)
 				}
 			}
 		}
