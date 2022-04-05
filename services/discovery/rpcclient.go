70123dcc2a06c4c94c89b7179369badd0bfd0788
Christian Rasmussen <72759223+rasmussen-untangle@users.noreply.github.com>
2022-04-05 11:51:14 -0600

    Mfw 1887 (#47)
    * Initial impl of RPC client stubs
    
    Co-authored-by: Christian Rasmussen <crasmussen@arista.com>

---
+++ services/discovery/rpcclient.go
@@ -0,0 +1,59 @@
+package discovery
+
+import (
+	"net/rpc"
+
+	"github.com/untangle/golang-shared/services/logger"
+	disco "github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
+)
+
+// RequestNetworkScan is a stub for the RPC call
+func RequestNetworkScan(args disco.ScanNetRequest) {
+	logger.Info("Requesting network scan\n")
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:5563")
+	if err != nil {
+		logger.Err("Failed to connect to discovery service: %s\n", err.Error())
+		return
+	}
+	defer client.Close()
+
+	var reply int
+	err = client.Call("DiscoveryRPCService.ScanNet", args, &reply)
+	if err != nil {
+		logger.Err("Failed to call DiscoveryRPCService.ScanNet", err)
+	}
+}
+
+// RequestHostScan is a stub for the RPC call
+func RequestHostScan(args disco.ScanHostRequest) {
+	logger.Info("Requesting host scan\n")
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:5563")
+	if err != nil {
+		logger.Err("Failed to connect to discovery service: %s\n", err.Error())
+		return
+	}
+	defer client.Close()
+
+	var reply int
+	err = client.Call("DiscoveryRPCService.ScanHost", args, &reply)
+	if err != nil {
+		logger.Err("Failed to call DiscoveryRPCService.ScanHost", err)
+	}
+}
+
+// RequestAllEntries is a stub for the RPC call
+func RequestAllEntries() {
+	logger.Info("Requesting all entries\n")
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:5563")
+	if err != nil {
+		logger.Err("Failed to connect to discovery service: %s\n", err.Error())
+		return
+	}
+	defer client.Close()
+
+	var reply int
+	err = client.Call("DiscoveryRPCService.RequestAllEntries", 0, &reply)
+	if err != nil {
+		logger.Err("Failed to call DiscoveryRPCService.RequestAllEntries", err)
+	}
+}
