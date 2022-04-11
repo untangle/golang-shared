15967e61f7fff8cfc1db0bdd8447ad69ba76c320
Christian Rasmussen <72759223+rasmussen-untangle@users.noreply.github.com>
2022-04-11 08:46:41 -0600

    Device entry merge function (#49)
    * export Merge
    
    * Fix find function
    
    Co-authored-by: Christian Rasmussen <crasmussen@arista.com>

--- services/discovery/types.go
+++ services/discovery/types.go
@@ -19,8 +19,26 @@ func (list *DevicesList) GetDeviceEntryFromIP(ip string) *disco.DiscoveryEntry {
 	list.Lock.RLock()
 	defer list.Lock.RUnlock()
 
-	if entry, ok := list.Devices[ip]; ok {
-		return &entry.Data
+	for _, entry := range list.Devices {
+		if entry.Data.IPv4Address == ip {
+			return &entry.Data
+		}
 	}
+
 	return nil
+}
+
+func (n *DeviceEntry) Merge(o DeviceEntry) {
+	if n.Data.IPv4Address != "" {
+		o.Data.IPv4Address = n.Data.IPv4Address
+	}
+	if n.Data.Lldp != nil {
+		o.Data.Lldp = n.Data.Lldp
+	}
+	if n.Data.Arp != nil {
+		o.Data.Arp = n.Data.Arp
+	}
+	if n.Data.Nmap != nil {
+		o.Data.Nmap = n.Data.Nmap
+	}
 }
