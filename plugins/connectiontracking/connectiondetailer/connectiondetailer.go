8e7bf24b5808b2e8bf8b34d04f71e30ec01b0e11
Adam Briles <abriles@arista.com>
2022-09-09 15:45:05 -0600

    removed redundant struct

--- plugins/connectiontracking/connectiondetailer/connectiondetailer.go
+++ plugins/connectiontracking/connectiondetailer/connectiondetailer.go
@@ -1,7 +1,9 @@
 package connectiondetailer
 
+import "github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
+
 // Interface for fetching system connection details
 type ConnectionDetailer interface {
-	GetConnectionList() ([]*ConnectionInfo, error)
+	GetConnectionList() ([]*Discoverd.ConnectionTracking, error)
 	FetchSystemConnections() error
 }
--- plugins/connectiontracking/connectiontracking.go
+++ plugins/connectiontracking/connectiontracking.go
@@ -5,7 +5,6 @@ import (
 	"github.com/untangle/discoverd/services/discovery"
 	disc "github.com/untangle/golang-shared/services/discovery"
 	"github.com/untangle/golang-shared/services/logger"
-	"github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
 )
 
 type ConnnectionTracking struct {
@@ -35,47 +34,13 @@ func (connectionTracking *ConnnectionTracking) ConnectionTrackingBackHandler(com
 	if fetchErr := connectionTracking.connectionDetails.FetchSystemConnections(); fetchErr == nil {
 		if connections, getErr := connectionTracking.connectionDetails.GetConnectionList(); getErr == nil {
 			for _, connection := range connections {
+
 				// Discovery entries can only be linked up by mac/ipv4
-				if connection.Original.Layer3.Protoname == "ipv4" {
+				if connection.Original.LayerThree.Protoname == "ipv4" {
 
 					entry := disc.DeviceEntry{}
 					entry.Init()
-					entry.Data.ConnectionTracking = &Discoverd.ConnectionTracking{}
-
-					// Set IPv4 in discovery entry
-					entry.Data.IPv4Address = connection.Original.Layer3.Src
-
-					entry.Data.ConnectionTracking.Independent = &Discoverd.Independent{}
-					entry.Data.ConnectionTracking.Independent.Id = connection.Independent.Id
-					entry.Data.ConnectionTracking.Independent.Mark = connection.Independent.Mark
-					entry.Data.ConnectionTracking.Independent.Timeout = connection.Independent.Timeout
-					entry.Data.ConnectionTracking.Independent.Use = connection.Independent.Use
-
-					entry.Data.ConnectionTracking.Original = &Discoverd.Original{}
-					entry.Data.ConnectionTracking.Original.LayerThree = &Discoverd.LayerThree{}
-					entry.Data.ConnectionTracking.Original.LayerFour = &Discoverd.LayerFour{}
-					entry.Data.ConnectionTracking.Original.LayerThree.Protoname = connection.Original.Layer3.Protoname
-					entry.Data.ConnectionTracking.Original.LayerThree.Protonum = connection.Original.Layer3.Protonum
-					entry.Data.ConnectionTracking.Original.LayerThree.Src = connection.Original.Layer3.Src
-					entry.Data.ConnectionTracking.Original.LayerThree.Dest = connection.Original.Layer3.Dst
-
-					entry.Data.ConnectionTracking.Original.LayerFour.Protoname = connection.Original.Layer4.Protoname
-					entry.Data.ConnectionTracking.Original.LayerFour.Protonum = connection.Original.Layer4.Protonum
-					entry.Data.ConnectionTracking.Original.LayerFour.SPort = connection.Original.Layer4.SPort
-					entry.Data.ConnectionTracking.Original.LayerFour.DPort = connection.Original.Layer4.DPort
-
-					entry.Data.ConnectionTracking.Reply = &Discoverd.Reply{}
-					entry.Data.ConnectionTracking.Reply.LayerThree = &Discoverd.LayerThree{}
-					entry.Data.ConnectionTracking.Reply.LayerFour = &Discoverd.LayerFour{}
-					entry.Data.ConnectionTracking.Reply.LayerThree.Protoname = connection.Reply.Layer3.Protoname
-					entry.Data.ConnectionTracking.Reply.LayerThree.Protonum = connection.Reply.Layer3.Protonum
-					entry.Data.ConnectionTracking.Reply.LayerThree.Src = connection.Reply.Layer3.Src
-					entry.Data.ConnectionTracking.Reply.LayerThree.Dest = connection.Reply.Layer3.Dst
-
-					entry.Data.ConnectionTracking.Reply.LayerFour.Protoname = connection.Reply.Layer4.Protoname
-					entry.Data.ConnectionTracking.Reply.LayerFour.Protonum = connection.Reply.Layer4.Protonum
-					entry.Data.ConnectionTracking.Reply.LayerFour.SPort = connection.Reply.Layer4.SPort
-					entry.Data.ConnectionTracking.Reply.LayerFour.DPort = connection.Reply.Layer4.DPort
+					entry.Data.ConnectionTracking = connection
 
 					logger.Debug("Created connection for %d\n", entry.Data.ConnectionTracking.Independent.Id)
 
@@ -84,6 +49,7 @@ func (connectionTracking *ConnnectionTracking) ConnectionTrackingBackHandler(com
 					discovery.UpdateDiscoveryEntry("", entry)
 
 				}
+
 			}
 		} else {
 			logger.Err("Couldn't get the connection list")
