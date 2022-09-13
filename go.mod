dbc140a31145b10f05ebbcde82a62e427ee15511
John Phillps <jphillips@arista.com>
2022-09-12 19:52:35 -0600

    work on discovery, upgrade to new golang-shared.

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/untangle/discoverd
 
 go 1.13
 
+replace github.com/untangle/golang-shared => /home/jphillips/code-projects/golang-shared
+
 require (
-	github.com/pebbe/zmq4 v1.2.8
+	github.com/go-playground/assert/v2 v2.2.0 // indirect
+	github.com/pebbe/zmq4 v1.2.9
+	github.com/stretchr/testify v1.8.0
 	github.com/untangle/golang-shared v0.9.9
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/protobuf v1.28.0
+	google.golang.org/protobuf v1.28.1
 )
--- plugins/arp/arp.go
+++ plugins/arp/arp.go
@@ -1,14 +1,21 @@
 package arp
 
 import (
+	"bufio"
+	"fmt"
 	"net"
+	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 
 	"github.com/untangle/discoverd/services/discovery"
 	disc "github.com/untangle/golang-shared/services/discovery"
 	"github.com/untangle/golang-shared/services/logger"
+	"github.com/untangle/golang-shared/services/settings"
+	"github.com/untangle/golang-shared/structs/interfaces"
 	"github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
+	disco_proto "github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
 )
 
 // Start starts the ARP collector
@@ -21,6 +28,129 @@ func Start() {
 
 // Stop stops QoS
 func Stop() {
+}
+
+// StringSet is a set of strings, backed by a map.
+type StringSet struct {
+	StringMap map[string]struct{}
+}
+
+// NewStringSet creates a new StringSet with no members.
+func NewStringSet() *StringSet {
+	return &StringSet{
+		StringMap: map[string]struct{}{},
+	}
+}
+
+// Add adds an item to the StringSet.
+func (set *StringSet) Add(item string) {
+	set.StringMap[item] = struct{}{}
+}
+
+// Contains tests for membership of item in StringSet.
+func (set *StringSet) Contains(item string) (contains bool) {
+	_, contains = set.StringMap[item]
+	return
+}
+
+// arpScanner scans a linux /proc/net/arp -format file.
+type arpScanner struct {
+	settings    *settings.SettingsFile
+	arpFileName string
+	entryList   []*disc.DeviceEntry
+	wans        *StringSet
+}
+
+func (scanner *arpScanner) buildWANList() error {
+	intfList := []*interfaces.Interface{}
+	if err := scanner.settings.UnmarshalSettingsAtPath(&intfList, "network", "interfaces"); err != nil {
+		return fmt.Errorf("buildWANList: couldn't unmarshall network settings: %w", err)
+	}
+
+	for _, intf := range intfList {
+		if intf.IsWAN {
+			scanner.wans.Add(intf.Device)
+		}
+	}
+	return nil
+
+}
+
+var ipv4Regex = `(\d+\.\d+\.\d+\.\d+)`
+var macRegex = `((?:[0-9A-Fa-f][0-9A-Fa-f]:){5,5}[0-9A-Fa-f][0-9A-Fa-f])`
+var hexRegex = `(0x\d+)`
+var maskRegex = `(\*)`
+var deviceRegex = `([a-zA-Z]+[a-zA-Z0-9]+)`
+var arpPattern = (ipv4Regex + `\s+` + // ipv4 is capture groups 2-3
+	hexRegex + `\s+` + // HW type is 4-5
+	hexRegex + `\s+` + // Flags is 6-7
+	macRegex + `\s+` + // mac is 8-9
+	maskRegex + `\s+` + // mask is 10-11
+	deviceRegex) // device is 12-13
+var arpLineRegex *regexp.Regexp = regexp.MustCompile(arpPattern)
+
+const (
+	arpIPGroupBegin     = 2
+	arpMacGroupBegin    = 8
+	arpDeviceGroupBegin = 12
+)
+
+func newArpScanner(settings *settings.SettingsFile,
+	arpFileName string) *arpScanner {
+	scanner := &arpScanner{
+		arpFileName: arpFileName,
+		settings:    settings,
+		wans:        NewStringSet(),
+	}
+
+	return scanner
+}
+
+func (scanner *arpScanner) scanLineForEntries(line []byte) {
+	indices := arpLineRegex.FindSubmatchIndex(line)
+	if len(indices) <= arpDeviceGroupBegin+1 {
+		return
+	}
+	ip := string(line[indices[arpIPGroupBegin]:indices[arpIPGroupBegin+1]])
+	mac := string(line[indices[arpMacGroupBegin]:indices[arpMacGroupBegin+1]])
+	if scanner.wans.Contains(string(line[indices[arpDeviceGroupBegin]:indices[arpDeviceGroupBegin+1]])) {
+		return
+	}
+
+	scanner.entryList = append(scanner.entryList,
+		&disc.DeviceEntry{
+			DiscoveryEntry: disco_proto.DiscoveryEntry{
+				MacAddress:  mac,
+				IPv4Address: ip,
+				Arp: &disco_proto.ARP{
+					Ip:  ip,
+					Mac: mac,
+				},
+			},
+		})
+}
+
+func (scanner *arpScanner) getArpEntriesFromFile() ([]*disc.DeviceEntry, error) {
+	scanner.entryList = []*disc.DeviceEntry{}
+	arp, err := os.Open(scanner.arpFileName)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't open arp file %s: %w", scanner.arpFileName, err)
+	}
+
+	if err := scanner.buildWANList(); err != nil {
+		return nil, fmt.Errorf("unable to build WAN list: %w", err)
+	}
+	fileScanner := bufio.NewScanner(arp)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Bytes()
+		scanner.scanLineForEntries(line)
+	}
+
+	if fileScanner.Err() != nil {
+		return nil, fmt.Errorf("couldn't scan arp file: %w", fileScanner.Err())
+	}
+	return scanner.entryList, nil
 }
 
 // ArpcallBackHandler is the callback handler for the ARP collector
@@ -42,18 +172,18 @@ func ArpcallBackHandler(commands []discovery.Command) {
 		// Initialize the entry
 		entry := disc.DeviceEntry{}
 		entry.Init()
-		entry.Data.Arp = &Discoverd.ARP{}
+		entry.Arp = &Discoverd.ARP{}
 
 		// Populate the entry
-		entry.Data.Arp.Ip = fields[0]
-		entry.Data.Arp.Mac = fields[3]
-		entry.Data.MacAddress = entry.Data.Arp.Mac
+		entry.Arp.Ip = fields[0]
+		entry.Arp.Mac = fields[3]
+		entry.MacAddress = entry.Arp.Mac
 
 		// Make sure the IP is valid before updating the entry, this also excludes headings
-		if net.ParseIP(entry.Data.Arp.Ip) != nil {
-			entry.Data.IPv4Address = entry.Data.Arp.Ip
-			entry.Data.MacAddress = entry.Data.Arp.Mac
-			discovery.UpdateDiscoveryEntry(entry.Data.Arp.Mac, entry)
+		if net.ParseIP(entry.Arp.Ip) != nil {
+			entry.IPv4Address = entry.Arp.Ip
+			entry.MacAddress = entry.Arp.Mac
+			discovery.UpdateDiscoveryEntry(entry.Arp.Mac, entry)
 		}
 	}
 }
