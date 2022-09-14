2753d231e18b5cc9f8d7fe11c0409998ab1112b7
John Phillps <jphillips@arista.com>
2022-09-14 12:03:08 -0600

    Simplify UpdateDiscoveryEntry.

--- services/discovery/deviceentry.go
+++ services/discovery/deviceentry.go
@@ -21,36 +21,6 @@ func UpdateDiscoveryEntry(mac string, entry *discovery.DeviceEntry) {
 		return
 	}
 
-	// mac = strings.ToLower(mac)
-	// entry.MacAddress = mac
-	// logger.Debug("Received %+v\n", entry)
-	// // If there is no Mac address, lets see if there is an existing entry with the IP address
-	// if mac == "" {
-	// 	if entry.IPv4Address != "" {
-	// 		existingEntry, ok := getDeviceEntryFromIP(entry.IPv4Address)
-	// 		if ok {
-	// 			entry.MacAddress = existingEntry.MacAddress
-	// 			mac = existingEntry.MacAddress
-	// 		} else {
-	// 			logger.Warn("No entry found for IP address %s, which is missing Mac Address. Can't add\n", entry.IPv4Address)
-	// 			return
-	// 		}
-	// 	}
-	// }
-	// // Do a check to see if mac is really a Mac Address
-	// if !isMacAddress(mac) {
-	// 	logger.Warn("UpdateDiscoveryEntry: Invalid MAC address: %s\n", mac)
-	// 	return
-	// }
-	// deviceListLock.Lock()
-	// if oldEntry, ok := deviceList[mac]; ok {
-	// 	// Merge the old entry with the new one
-	// 	entry.Merge(&oldEntry)
-	// }
-
-	// deviceList[mac] = entry
-	// deviceListLock.Unlock()
-
 	// ZMQ publish the entry
 	entry.LastUpdate = time.Now().Unix()
 	logger.Debug("Publishing discovery entry for %s, %s\n", mac, entry.IPv4Address)
@@ -65,35 +35,3 @@ func zmqpublishEntry(entry *discovery.DeviceEntry) {
 	}
 	messagePublisherChannel <- &zmqMessage{"arista:discovery:device", message}
 }
-
-// func publishAll() {
-// 	deviceListLock.RLock()
-// 	for _, entry := range deviceList {
-// 		zmqpublishEntry(entry)
-// 	}
-// 	deviceListLock.RUnlock()
-// }
-
-// func isMacAddress(mac string) bool {
-// 	var validChars = "0123456789abcdefABCDEF:"
-// 	if len(mac) != 17 {
-// 		return false
-// 	}
-// 	for _, c := range mac {
-// 		if !strings.Contains(validChars, string(c)) {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
-
-// func getDeviceEntryFromIP(ip string) (discovery.DeviceEntry, bool) {
-// 	deviceListLock.RLock()
-// 	for _, entry := range deviceList {
-// 		if entry.IPv4Address == ip {
-// 			deviceListLock.RUnlock()
-// 			return entry, true
-// 		}
-// 	}
-// 	return discovery.DeviceEntry{}, false
-// }
