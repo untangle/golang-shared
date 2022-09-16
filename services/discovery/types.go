555cea8b47531965ff5113421c7fa6bb28429b70
Adam Briles <abriles@arista.com>
2022-09-16 12:52:24 -0600

    added logs for testing

--- services/discovery/types.go
+++ services/discovery/types.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/untangle/golang-shared/services/logger"
 	disco "github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
 	"google.golang.org/protobuf/proto"
 )
@@ -151,6 +152,11 @@ func (n *DeviceEntry) Merge(o *DeviceEntry) {
 	}
 	if n.Nmap == nil {
 		n.Nmap = o.Nmap
+	}
+
+	logger.Err("Merging something")
+	if o.Connections != nil {
+		logger.Err("Merging something with connections")
 	}
 
 	// Since connections change on a device over time, overwrite the original ConnectionTracking entry
