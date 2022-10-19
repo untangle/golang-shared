c9dbade91150b14460a883de39d1b0d8cfab35f6
John Phillps <jphillips@arista.com>
2022-10-19 08:53:39 -0600

    Add signal handler.

---
+++ plugins/signal_handler.go
@@ -0,0 +1,36 @@
+package plugins
+
+import (
+	"syscall"
+
+	"github.com/untangle/golang-shared/services/logger"
+)
+
+type SignalHandlingPlugin interface {
+	Signal(syscall.Signal) error
+}
+
+type SignalHandler struct {
+	plugins []SignalHandlingPlugin
+}
+
+func NewSignalHandler() *SignalHandler {
+	return &SignalHandler{}
+}
+
+func (handler *SignalHandler) RegisterPlugin(plugin SignalHandlingPlugin) {
+	handler.plugins = append(handler.plugins, plugin)
+}
+
+// Signal calls the Signal() method of all registered plugins with sig.
+func (handler *SignalHandler) Signal(sig syscall.Signal) {
+	for _, sigHandler := range handler.plugins {
+		if err := sigHandler.Signal(sig); err != nil {
+			plugin := sigHandler.(Plugin)
+			logger.Warn("Plugin %s returned error handling signal %v: %s\n",
+				plugin.Name(),
+				sig,
+				err)
+		}
+	}
+}
