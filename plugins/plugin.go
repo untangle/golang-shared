00e2bd7fc70fbc1dcf502ce1d2ae91822ba5ec22
Adam Briles <abriles@arista.com>
2022-10-24 11:21:44 -0600

    debugging

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -124,6 +124,9 @@ func (control *PluginControl) Startup() {
 			panic(fmt.Sprintf("couldn't instantiate plugin: %s", err))
 		}
 	}
+
+	logger.Err("The number of plugins is %d \n", len(control.plugins))
+
 	for _, plugin := range control.plugins {
 		logger.Info("Starting plugin: %s\n", plugin.Name())
 		if err := plugin.Startup(); err != nil {
