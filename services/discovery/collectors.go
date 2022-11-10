cbc4f7ab8f50e7f0315681da0e9bf18cb870095a
AlexandruBungiu <113093757+AlexandruBungiu@users.noreply.github.com>
2022-11-10 19:47:19 +0200

    MFW-2364 separate gRPC request for collectors (#77)
    * added Collectors enum; renamed NmapRequest/Response to a more generic ScanRequest/Response;
    
    * more generic arg name for ScanRequest
    
    * removed collectors enum; consts for rpcClient;
    
    * start from 1 like a normal person
    
    * added another layer on top of the proto generated types; renamed some stuff so they are more suggestive;
    
    * -
    
    * more comments
    
    * renamed some stuff
    
    * simplified FillDeviceListWithZMQDeviceMessages a bit

---
+++ services/discovery/collectors.go
@@ -0,0 +1,50 @@
+package discovery
+
+import (
+	disco "github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
+)
+
+// CollectorName - type alias used for identifying collector plugins
+type CollectorName string
+
+const (
+	All CollectorName = "all"
+
+	Discovery CollectorName = "discovery"
+
+	Arp       CollectorName = "arp" // will be replaced by Neighbour
+	Lldp      CollectorName = "lldp"
+	Neighbour CollectorName = "neighbour"
+	Nmap      CollectorName = "nmap"
+)
+
+// CallCollectorsRequest - request for the CallCollectors RPC stub, a stronger-typed wrapper for the RPC func's request
+type CallCollectorsRequest struct {
+	Collectors []CollectorName
+	Args       []string
+}
+
+// toRpcRequest - converts the wrapper request struct to the one the RPC function expects
+func toRpcRequest(req CallCollectorsRequest) disco.CallDiscoveryRequest {
+	strCollectors := make([]string, len(req.Collectors), len(req.Collectors))
+	for i, c := range req.Collectors {
+		strCollectors[i] = string(c)
+	}
+
+	return disco.CallDiscoveryRequest{
+		Collectors: strCollectors,
+		Args:       req.Args,
+	}
+}
+
+// CallCollectorsResponse - CallCollectors RPC stub response, wrapper for the RPC func's response
+type CallCollectorsResponse struct {
+	Result int32
+}
+
+// fromRPCResponse - converts the RPC response into a wrapper
+func fromRPCResponse(rpcResponse disco.CallDiscoveryResponse) CallCollectorsResponse {
+	return CallCollectorsResponse{
+		int32(rpcResponse.Result),
+	}
+}
--- services/discovery/rpcclient.go
+++ services/discovery/rpcclient.go
@@ -6,36 +6,33 @@ import (
 	disco "github.com/untangle/golang-shared/structs/protocolbuffers/Discoverd"
 )
 
-// RequestNetworkScan is a stub for the RPC call
-func RequestNetworkScan(args disco.NmapRequest) {
-	logger.Info("Requesting network scan\n")
-	client, err := rpc.DialHTTP("tcp", "127.0.0.1:5563")
+const (
+	network = "tcp"
+	address = "127.0.0.1:5563"
+)
+
+// CallCollectors is a stub for the RPC call
+func CallCollectors(args CallCollectorsRequest) (*CallCollectorsResponse, error) {
+	logger.Info("CallCollectors called\n")
+	if len(args.Collectors) == 0 {
+		logger.Warn("CallCollectors called but no collector specified!")
+	}
+
+	client, err := rpc.DialHTTP(network, address)
 	if err != nil {
 		logger.Err("Failed to connect to discovery service: %s\n", err.Error())
-		return
+		return nil, err
 	}
 	defer client.Close()
 
-	var reply disco.NmapResponse
-	err = client.Call("DiscoveryRPCService.ScanNet", args, &reply)
-	if err != nil {
-		logger.Err("Failed to call DiscoveryRPCService.ScanNet %s\n", err.Error())
-	}
-}
+	rpcRequest := toRpcRequest(args)
+	var rpcResponse disco.CallDiscoveryResponse
 
-// RequestHostScan is a stub for the RPC call
-func RequestHostScan(args disco.NmapRequest) {
-	logger.Info("Requesting host scan\n")
-	client, err := rpc.DialHTTP("tcp", "127.0.0.1:5563")
-	if err != nil {
-		logger.Err("Failed to connect to discovery service: %s\n", err.Error())
-		return
+	if err := client.Call("DiscoveryRPCService.CallDiscovery", &rpcRequest, &rpcResponse); err != nil {
+		logger.Err("Failed to call DiscoveryRPCService.CallDiscovery %s\n", err.Error())
+		return nil, err
 	}
-	defer client.Close()
 
-	var reply disco.NmapResponse
-	err = client.Call("DiscoveryRPCService.ScanHost", args, &reply)
-	if err != nil {
-		logger.Err("Failed to call DiscoveryRPCService.ScanHost %s\n", err.Error())
-	}
+	response := fromRPCResponse(rpcResponse)
+	return &response, nil
 }
