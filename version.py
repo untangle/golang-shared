7e066066948706916e7dbaf7f268cf733af1d17d
John Phillps <jphillips@arista.com>
2022-11-09 17:06:13 -0700

    Clean up versioning script.

--- version.py
+++ version.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+"""
+This is a script to output the new version tag on stdout. It assumes
+it is run in the git repo, and the latest commit message text is provided to
+the program's STDIN. We search the stdin for a pattern saying whether
+to increment version and if so, which one (major, minor, bug).
+"""
 import subprocess
 import re
 import sys
@@ -11,6 +18,9 @@ patterns = {
 
 
 def tag2version(tag: str):
+    """Convert a tag string to a version tuple,
+    returns a three-tuple of integers or None if the tag string wasn't
+    a proper tag."""
     output = re.match(tag_reg, tag)
     if output is not None:
         return (int(output.group(1)),
@@ -19,8 +29,13 @@ def tag2version(tag: str):
     return None
 
 
-def find_latest_tag():
-    subprocess.run(['git', 'fetch', 'origin', '--tags'], check=True)
+def find_latest_tag(fetch: bool):
+    """
+    find the most recent/highest numbered tag. If fetch is True, run
+    git fetch origin --tags first so we know what the tags are.
+    """
+    if fetch:
+        subprocess.run(['git', 'fetch', 'origin', '--tags'], check=True)
     tags = [tag2version(i) for i in
             (subprocess.run(['git', 'tag'], stdout=subprocess.PIPE,
                             check=True)
@@ -29,9 +44,12 @@ def find_latest_tag():
     return latest
 
 
-latest = find_latest_tag()
+do_fetch = '--fetch' in sys.argv[1:]
+latest = find_latest_tag(do_fetch)
 msg = sys.stdin.read().splitlines()
 for type, (pattern, index) in patterns.items():
+    # if any line matches the version increment pattern, increment the
+    # version and exit.
     if any((re.match(pattern, i) for i in msg)):
         new = list(latest)
         new[index] += 1
