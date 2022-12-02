67e0f2771a51a9b53ba6c28104e38796dbe8d281
Indira Ramasamy <iramasamy@arista.com>
2022-12-02 10:48:19 -0700

    MFW-2347: Handle IP aliases from discovery
    version:bug

--- util/net/interfaces.go
+++ util/net/interfaces.go
@@ -16,35 +16,66 @@ type Interface struct {
 	DHCPRangeStart    string
 	DHCPRangeEnd      string
 	DownloadKbps      int
-	Enabled           bool   `json:"enabled"`
-	Name              string `json:"name"`
-	Type              string `json:"type"`
-	V4StaticAddress   string `json:"v4StaticAddress"`
-	V4StaticPrefix    uint8  `json:"v4StaticPrefix"`
-	V6StaticAddress   string `json:"v6StaticAddress"`
-	V6StaticPrefix    uint8  `json:"v6StaticPrefix"`
-	IsWAN             bool   `json:"wan"`
+
+	Enabled         bool          `json:"enabled"`
+	Name            string        `json:"name"`
+	Type            string        `json:"type"`
+	V4StaticAddress string        `json:"v4StaticAddress"`
+	V4StaticPrefix  uint8         `json:"v4StaticPrefix"`
+	V4Aliases       []V4IpAliases `json:"v4Aliases"`
+	V6StaticAddress string        `json:"v6StaticAddress"`
+	V6StaticPrefix  uint8         `json:"v6StaticPrefix"`
+	V6Aliases       []V6IpAliases `json:"v6Aliases"`
+	IsWAN           bool          `json:"wan"`
+}
+type V4IpAliases struct {
+	V4Address string `json:"v4Address"`
+	V4Prefix  uint32 `json:"v4Prefix"`
+}
+type V6IpAliases struct {
+	V6Address string `json:"v6Address"`
+	V6Prefix  string `json:"v6Prefix"`
 }
 
-func (intf *Interface) GetIpV4Network() (*net.IPNet, error) {
+func (intf *Interface) GetIpV4Network() []net.IPNet {
+	var networks []net.IPNet
 	_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%d", intf.V4StaticAddress, intf.V4StaticPrefix))
-	return ipNet, err
+	if err == nil {
+		networks = append(networks, *ipNet)
+	}
+	for _, alias := range intf.V4Aliases {
+		_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%d", alias.V4Address, alias.V4Prefix))
+		if err == nil {
+			networks = append(networks, *ipNet)
+		}
+	}
+	return networks
 }
 
-func (intf *Interface) GetIpV6Network() (*net.IPNet, error) {
+func (intf *Interface) GetIpV6Network() []net.IPNet {
+	var networks []net.IPNet
 	_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%d", intf.V6StaticAddress, intf.V6StaticPrefix))
-	return ipNet, err
+	if err == nil {
+		networks = append(networks, *ipNet)
+	}
+	for _, alias := range intf.V6Aliases {
+		_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%s", alias.V6Address, alias.V6Prefix))
+		if err == nil {
+			networks = append(networks, *ipNet)
+		}
+	}
+	return networks
 }
 
 func (intf *Interface) GetNetworks() []net.IPNet {
 	var networks []net.IPNet
-	ipV4Net, v4Err := intf.GetIpV4Network()
-	if v4Err == nil {
-		networks = append(networks, *ipV4Net)
+	ipV4Nets := intf.GetIpV4Network()
+	if len(ipV4Nets) != 0 {
+		networks = append(networks, ipV4Nets...)
 	}
-	ipV6Net, v6Err := intf.GetIpV6Network()
-	if v6Err == nil {
-		networks = append(networks, *ipV6Net)
+	ipV6Nets := intf.GetIpV6Network()
+	if len(ipV6Nets) != 0 {
+		networks = append(networks, ipV6Nets...)
 	}
 	return networks
 }
