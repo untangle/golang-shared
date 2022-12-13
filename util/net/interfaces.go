7ee21fa2ef564b66f87fc74117a174f09266bb0d
john-phillips-arista <106119552+john-phillips-arista@users.noreply.github.com>
2022-12-13 11:47:40 -0700

    Update some bridge-related stuff. (#113)
    - Make the network interfaces settings tools more OO
    - Add utility methods and fields to the Interfaces struct.
    - Add a Most-specific-prefix utility func.

--- util/net/interfaces.go
+++ util/net/interfaces.go
@@ -5,6 +5,12 @@ import (
 	"net"
 )
 
+const (
+	// ConfigTypeBridged - If the interface is a bridged
+	// interface, this is it's ConfigType.
+	ConfigTypeBridged = "BRIDGED"
+)
+
 // Interface corresponds to the interface JSON structure in the settings.json
 // file.
 type Interface struct {
@@ -19,6 +25,7 @@ type Interface struct {
 
 	Enabled         bool          `json:"enabled"`
 	Name            string        `json:"name"`
+	InterfaceID     int           `json:"interfaceID"`
 	Type            string        `json:"type"`
 	V4StaticAddress string        `json:"v4StaticAddress"`
 	V4StaticPrefix  uint8         `json:"v4StaticPrefix"`
@@ -37,41 +44,45 @@ type V6IpAliases struct {
 	V6Prefix  string `json:"v6Prefix"`
 }
 
+func (intf *Interface) IsBridged() bool {
+	return intf.ConfigType == ConfigTypeBridged
+}
+
 // Get IPV4 static and aliases addresses
-func (intf *Interface) GetIpV4Network() []net.IPNet {
-	var networks []net.IPNet
+func (intf *Interface) GetIpV4Network() []*net.IPNet {
+	var networks []*net.IPNet
 	_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%d", intf.V4StaticAddress, intf.V4StaticPrefix))
 	if err == nil {
-		networks = append(networks, *ipNet)
+		networks = append(networks, ipNet)
 	}
 	for _, alias := range intf.V4Aliases {
 		_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%d", alias.V4Address, alias.V4Prefix))
 		if err == nil {
-			networks = append(networks, *ipNet)
+			networks = append(networks, ipNet)
 		}
 	}
 	return networks
 }
 
 // Get IPV6 static and aliases addresses
-func (intf *Interface) GetIpV6Network() []net.IPNet {
-	var networks []net.IPNet
+func (intf *Interface) GetIpV6Network() []*net.IPNet {
+	var networks []*net.IPNet
 	_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%d", intf.V6StaticAddress, intf.V6StaticPrefix))
 	if err == nil {
-		networks = append(networks, *ipNet)
+		networks = append(networks, ipNet)
 	}
 	for _, alias := range intf.V6Aliases {
 		_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%s", alias.V6Address, alias.V6Prefix))
 		if err == nil {
-			networks = append(networks, *ipNet)
+			networks = append(networks, ipNet)
 		}
 	}
 	return networks
 }
 
 // Get IPV4 and IPV6 static addresses and aliases addresses
-func (intf *Interface) GetNetworks() []net.IPNet {
-	var networks []net.IPNet
+func (intf *Interface) GetNetworks() []*net.IPNet {
+	var networks []*net.IPNet
 	ipV4Nets := intf.GetIpV4Network()
 	if len(ipV4Nets) != 0 {
 		networks = append(networks, ipV4Nets...)
@@ -83,10 +94,13 @@ func (intf *Interface) GetNetworks() []net.IPNet {
 	return networks
 }
 
-func (intf *Interface) HasContainingNetwork(addr net.IP) net.IPNet {
+// MostSpecificPrefixMatch returns from the networks and addr the
+// network with the longest matching prefix.
+func MostSpecificPrefixMatch(nets []*net.IPNet, addr net.IP) *net.IPNet {
 	currMask := 0
-	var maxMatching net.IPNet
-	for _, network := range intf.GetNetworks() {
+	var maxMatching *net.IPNet = nil
+
+	for _, network := range nets {
 		// do most specific prefix match on networks belonging to this interface
 		ones, _ := network.Mask.Size()
 		if network.Contains(addr) && ones > currMask {
@@ -96,4 +110,8 @@ func (intf *Interface) HasContainingNetwork(addr net.IP) net.IPNet {
 		}
 	}
 	return maxMatching
+}
+
+func (intf *Interface) HasContainingNetwork(addr net.IP) *net.IPNet {
+	return MostSpecificPrefixMatch(intf.GetNetworks(), addr)
 }
