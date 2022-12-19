4ac6322493ef6a40b777ecde634a53231a5729af
Adam Briles <abriles@arista.com>
2022-12-19 11:41:46 -0700

    added some helper functions to types.go

--- services/discovery/types.go
+++ services/discovery/types.go
@@ -447,3 +447,68 @@ func (n *DeviceEntry) SetMac(mac string) {
 	n.MacAddress = strings.ToLower(mac)
 
 }
+
+// Normalizes the data in each collector entry
+// Returns an error if the data couldn't be normalized or
+// 	if the provided argument isn't a pointer to a collector struct
+func NormalizeCollectorEntry(collector interface{}) error {
+	switch collectorWithType := collector.(type) {
+	case *disco.LLDP:
+		collectorWithType.Ip = strings.ToUpper(collectorWithType.Ip)
+		collectorWithType.Mac = strings.ToUpper(collectorWithType.Mac)
+	case *disco.NEIGH:
+		collectorWithType.Ip = strings.ToUpper(collectorWithType.Ip)
+		collectorWithType.Mac = strings.ToUpper(collectorWithType.Mac)
+	case *disco.NMAP:
+		collectorWithType.Ip = strings.ToUpper(collectorWithType.Ip)
+		collectorWithType.Mac = strings.ToUpper(collectorWithType.Mac)
+	default:
+		return fmt.Errorf("provided argument was not a pointer to a collector struct")
+	}
+
+	return nil
+}
+
+// Wraps an LLDP, NMAP, or NEIGH collector struct in a device entry
+// If the collector struct is missing a field needed to initialize the
+// 	collector struct, and error is returned.
+func WrapCollectorInDeviceEntry(collector interface{}) (*DeviceEntry, error) {
+	var deviceEntry DeviceEntry
+
+	switch collectorWithType := collector.(type) {
+	case *disco.LLDP:
+		if collectorWithType.Ip == "" {
+			return nil, fmt.Errorf("LLDP entry missing IP field")
+		}
+
+		deviceEntry.MacAddress = collectorWithType.Mac
+
+		deviceEntry.Lldp = make(map[string]*disco.LLDP)
+		deviceEntry.Lldp[collectorWithType.Ip] = collectorWithType
+
+	case *disco.NEIGH:
+		if collectorWithType.Ip == "" {
+			return nil, fmt.Errorf("NEIGH entry missing IP field")
+		}
+
+		deviceEntry.MacAddress = collectorWithType.Mac
+
+		deviceEntry.Neigh = make(map[string]*disco.NEIGH)
+		deviceEntry.Neigh[collectorWithType.Ip] = collectorWithType
+
+	case *disco.NMAP:
+		if collectorWithType.Ip == "" {
+			return nil, fmt.Errorf(("NMAP entry missing IP field"))
+		}
+
+		deviceEntry.MacAddress = collectorWithType.Mac
+
+		deviceEntry.Nmap = make(map[string]*disco.NMAP)
+		deviceEntry.Nmap[collectorWithType.Ip] = collectorWithType
+
+	default:
+		return nil, fmt.Errorf("argument provided to the function was not an LLDP, NMAP, or NEIGH struct")
+	}
+
+	return &deviceEntry, nil
+}
