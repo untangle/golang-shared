2bf842a86e9a36cc8d8f29494ebca5b68953d565
Christian Rasmussen <crasmussen@arista.com>
2023-01-18 08:46:16 -0700

    Add ipV6

--- util/net/interfaces/interfaces.go
+++ util/net/interfaces/interfaces.go
@@ -70,6 +70,9 @@ func (intf *Interface) GetIpV4Network() []*net.IPNet {
 // Get IPV6 static and aliases addresses
 func (intf *Interface) GetIpV6Network() []*net.IPNet {
 	var networks []*net.IPNet
+	if intf.IsBridged() {
+		return networks
+	}
 	_, ipNet, err := net.ParseCIDR(fmt.Sprintf("%s/%d", intf.V6StaticAddress, intf.V6StaticPrefix))
 	if err == nil {
 		networks = append(networks, ipNet)
