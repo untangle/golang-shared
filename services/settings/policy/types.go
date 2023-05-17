299feb4492e4ae5bb60aa19ae73314665e4fb62a
John Phillps <jphillips@arista.com>
2023-05-17 17:26:59 -0600

    Some updates for merge.

--- services/settings/policy/types.go
+++ services/settings/policy/types.go
@@ -43,13 +43,13 @@ type PolicyConfiguration struct {
 
 // Policies are the root of our policy configurations. It includes pointers to substructure.
 type Policy struct {
-	Defaults       bool      `json:"defaults"`
-	ID             string    `json:"id"`
-	Name           string    `json:"name"`
-	Description    string    `json:"description"`
-	Enabled        bool      `json:"enabled"`
-	Configurations []*string `json:"configurations"`
-	Flows          []*string `json:"flows"`
+	Defaults       bool     `json:"defaults"`
+	ID             string   `json:"id"`
+	Name           string   `json:"name"`
+	Description    string   `json:"description"`
+	Enabled        bool     `json:"enabled"`
+	Configurations []string `json:"configurations"`
+	Flows          []string `json:"flows"`
 }
 
 // Equal compares the leftSide to the PolicyConditionType
