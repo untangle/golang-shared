8c447b3ef4d2ae811d33625e91ce6ef42557ca03
Manuwela Kanade <manuwela.kanade@gmail.com>
2023-06-22 23:42:24 +0530

    MFW-3135 Fixing comment

--- services/settings/settings_file.go
+++ services/settings/settings_file.go
@@ -229,7 +229,7 @@ func (file *SettingsFile) SetAllSettingsWithExceptions(newSettings map[string]in
 	}
 
 	// Default exception: Web admin ports will be set to current settings
-	logger.Info("System settings for web admin ports will not be restored")
+	logger.Info("System settings for web admin ports will not be restored\n")
 	newSettings["system"].(map[string]interface{})["httpPort"] = currentSettings["system"].(map[string]interface{})["httpPort"].(string)
 	newSettings["system"].(map[string]interface{})["httpsPort"] = currentSettings["system"].(map[string]interface{})["httpsPort"].(string)
 
