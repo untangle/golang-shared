92896c94be2c6371949f8bd519d63ef31a7d96ce
Preetham Thava <pthava@arista.com>
2023-06-23 11:58:44 -0600

    Added data structure for dynamic IP blocker config

---
+++ services/settings/dynamic_ip_blocker/types.go
@@ -0,0 +1,23 @@
+package dynamic_ip_blocker
+
+// Example for this struct is present on mfw_schema
+
+// DynamicIpBlockerConfig is the data structure for Dynamic IP blocker configurations.
+// It contains an array of Dynamic IP Blocker Configurations
+type DynamicIpBlockerConfig struct {
+	Name           string   `json:"name"`
+	ID             string   `json:"id"`
+	Type           string   `json:"type"`
+	Enable         bool     `json:"enable"`
+	Source         string   `json:"source"`
+	RegexType      string   `json:"regexType"`
+	UpdateInterval Interval `json:"interval"`
+}
+
+// This is the Interval data structure, which stores the time interval between two update attempts.
+type Interval struct {
+	Enabled bool `json:"enabled"`
+	DayOfWeek int `json:"dayOfWeek"`
+	HourOfday int `json:"hourOfday"`
+	MinuteOfhour int `json:"minuteOfhour"`
+}
