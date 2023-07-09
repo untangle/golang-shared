be354722243c0b772262988b55853188bcff4c4f
Al Parker <al.parker@arista.com>
2023-07-09 16:57:46 -0400

    MFW-3105 Fixed addresses and test

--- util/net/addrs.go
+++ util/net/addrs.go
@@ -1,9 +1,9 @@
 package net
 
 import (
-	"bytes"
+	"errors"
 	"fmt"
-	"net"
+	"net/netip"
 	"strings"
 )
 
@@ -19,10 +19,10 @@ const (
 )
 
 // CheckIPAddressType returns the type of the IP address.
-func CheckIPAddressType(ip net.IP) (string, error) {
-	if ip.To4() != nil {
+func CheckIPAddressType(ip netip.Addr) (string, error) {
+	if ip.Is4() {
 		return IPv4Str, nil
-	} else if ip.To16() != nil {
+	} else if ip.Is6() {
 		return IPv6Str, nil
 	} else {
 		return "", fmt.Errorf("InvalidIPStr")
@@ -34,16 +34,15 @@ type IPSpecifierString string
 
 // IPRange is a range of IPs, from Start to End inclusive.
 type IPRange struct {
-	Start net.IP
-	End   net.IP
+	Start netip.Addr
+	End   netip.Addr
 }
 
 // Contains returns true if the ip is between the Start and End of r,
 // inclusive.
-func (r IPRange) Contains(ip net.IP) bool {
-	return bytes.Compare(r.Start, ip) <= 0 &&
-		bytes.Compare(r.End, ip) >= 0
-
+func (r IPRange) Contains(ip netip.Addr) bool {
+	return r.Start.Compare(ip) <= 0 &&
+		r.End.Compare(ip) >= 0
 }
 
 // Parse returns the parsed specifier as one of:
@@ -58,51 +57,68 @@ func (ss IPSpecifierString) Parse() any {
 			return fmt.Errorf("invalid ip specifier string range, contains too many -: %s",
 				ss)
 		}
-		start := net.ParseIP(parts[0])
-		end := net.ParseIP(parts[1])
-
-		if start == nil || end == nil {
+		start, err := netip.ParseAddr(parts[0])
+		if err != nil {
 			return fmt.Errorf("invalid ip specifier string range, contains bad IPs: %s",
 				ss)
 		}
-
-		if bytes.Compare(start, end) > 0 {
+		end, err := netip.ParseAddr(parts[1])
+		if err != nil {
+			return fmt.Errorf("invalid ip specifier string range, contains bad IPs: %s",
+				ss)
+		}
+		if start.Compare(end) > 0 {
 			return fmt.Errorf("invalid IP range, start > end: %s", ss)
 		}
 
 		return IPRange{Start: start, End: end}
 	} else if strings.Contains(string(ss), "/") {
-		if _, network, err := net.ParseCIDR(string(ss)); err != nil {
+		if network, err := netip.ParsePrefix(string(ss)); err != nil {
 			return err
 		} else {
 			return network
 		}
 
-	} else if ip := net.ParseIP(string(ss)); ip != nil {
+	} else if ip, err := netip.ParseAddr(string(ss)); err == nil {
 		return ip
 	} else {
 		return fmt.Errorf("invalid ip specifier: %s", ss)
 	}
 }
 
-// NetToRange converts a *net.IPNet to an IPRange.
-func NetToRange(network *net.IPNet) IPRange {
-	masked := network.IP.Mask(network.Mask)
-	lower := make(net.IP, len(network.IP))
-	upper := make(net.IP, len(network.IP))
-	copy(lower, masked)
-	copy(upper, masked)
-	ones, bits := network.Mask.Size()
-	maskedBytes := (bits - ones) / 8
-	remainderBits := (bits - ones) % 8
+// NetToRange converts a *netip.IPNet to an IPRange.
+func NetToRange(prefix netip.Prefix) (IPRange, error) {
 
-	for i := 1; i <= maskedBytes; i++ {
-		upper[len(masked)-i] = 0xff
+	if !prefix.IsValid() {
+		return IPRange{}, errors.New("invalid prefix")
+	}
+	maskBits := prefix.Bits()
+	if prefix.Addr().Is4In6() && maskBits < 96 {
+		return IPRange{}, errors.New("prefix with 4in6 address must have mask >= 96")
+	}
+	base := prefix.Masked().Addr()
+
+	// the internal 128bit representation is privat
+	// all calculations must be done in the bytes representation
+	a16 := base.As16()
+
+	if base.Is4() {
+		maskBits += 96
 	}
 
-	if remainderBits != 0 {
-		remainderMask := (1 << (remainderBits)) - 1
-		upper[len(masked)-(maskedBytes+1)] |= byte(remainderMask)
+	// set host bits to 1
+	for b := maskBits; b < 128; b++ {
+		byteNum, bitInByte := b/8, 7-(b%8)
+		a16[byteNum] |= 1 << uint(bitInByte)
 	}
-	return IPRange{Start: lower, End: upper}
+
+	// back to internal 128bit representation
+	last := netip.AddrFrom16(a16)
+
+	// unmap last to v4 if base is v4
+	if base.Is4() {
+		last = last.Unmap()
+	}
+
+	return IPRange{base, last}, nil
 }
--- util/net/addrs_test.go
+++ util/net/addrs_test.go
@@ -1,7 +1,7 @@
 package net
 
 import (
-	"net"
+	"net/netip"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,15 +14,16 @@ func TestIPSpecString(t *testing.T) {
 		shoudlerr   bool
 		shouldequal any
 	}{
-		{"ipv4 address", "132.123.123.1", false, net.IPv4(132, 123, 123, 1)},
+		{"ipv4 address", "132.123.123.1", false, netip.AddrFrom4([4]byte{132, 123, 123, 1})},
 		{"ipv4 net", "132.123.123.1/24", false,
-			func() *net.IPNet {
-				_, net, _ := net.ParseCIDR("132.123.123.1/24")
+			func() netip.Prefix {
+				net, _ := netip.ParsePrefix("132.123.123.1/24")
 				return net
 			}(),
 		},
 		{"ipv4 range", "132.123.123.1-132.123.123.3", false,
-			IPRange{Start: net.IPv4(132, 123, 123, 1), End: net.IPv4(132, 123, 123, 3)},
+			IPRange{Start: netip.AddrFrom4([4]byte{132, 123, 123, 1}),
+				End: netip.AddrFrom4([4]byte{132, 123, 123, 3})},
 		},
 		{"ipv4 range, start less than end", "132.123.123.1-132.123.123.0", true, nil},
 		{"ipv4 range, too many dashes", "132.123.123.1--132.123.123.20", true, nil},
@@ -35,7 +36,7 @@ func TestIPSpecString(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			val := IPSpecifierString(tt.stringval).Parse()
 			switch typed := val.(type) {
-			case net.IP, *net.IPNet, IPRange:
+			case netip.Addr, netip.Prefix, IPRange:
 				assert.EqualValues(t, typed, tt.shouldequal)
 			case error:
 				assert.True(t, tt.shoudlerr)
@@ -50,37 +51,37 @@ func TestIPRange(t *testing.T) {
 	tests := []struct {
 		name          string
 		ipRange       IPRange
-		ipAddr        net.IP
+		ipAddr        netip.Addr
 		shouldContain bool
 	}{
 		{
 			"basic in",
-			IPRange{net.IPv4(0, 0, 0, 0), net.IPv4(1, 0, 0, 0)},
-			net.IPv4(0, 1, 0, 0),
+			IPRange{netip.AddrFrom4([4]byte{0, 0, 0, 0}), netip.AddrFrom4([4]byte{1, 0, 0, 0})},
+			netip.AddrFrom4([4]byte{0, 1, 0, 0}),
 			true,
 		},
 		{
 			"basic out",
-			IPRange{net.IPv4(0, 0, 0, 0), net.IPv4(1, 0, 0, 0)},
-			net.IPv4(1, 1, 0, 0),
+			IPRange{netip.AddrFrom4([4]byte{0, 0, 0, 0}), netip.AddrFrom4([4]byte{1, 0, 0, 0})},
+			netip.AddrFrom4([4]byte{1, 1, 0, 0}),
 			false,
 		},
 		{
 			"basic upper border",
-			IPRange{net.IPv4(0, 0, 0, 0), net.IPv4(1, 0, 0, 0)},
-			net.IPv4(1, 0, 0, 0),
+			IPRange{netip.AddrFrom4([4]byte{0, 0, 0, 0}), netip.AddrFrom4([4]byte{1, 0, 0, 0})},
+			netip.AddrFrom4([4]byte{1, 0, 0, 0}),
 			true,
 		},
 		{
 			"basic lower border",
-			IPRange{net.IPv4(0, 0, 0, 0), net.IPv4(1, 0, 0, 0)},
-			net.IPv4(0, 0, 0, 0),
+			IPRange{netip.AddrFrom4([4]byte{0, 0, 0, 0}), netip.AddrFrom4([4]byte{1, 0, 0, 0})},
+			netip.AddrFrom4([4]byte{0, 0, 0, 0}),
 			true,
 		},
 		{
 			"basic out, lower",
-			IPRange{net.IPv4(1, 0, 0, 0), net.IPv4(2, 0, 0, 0)},
-			net.IPv4(0, 0, 0, 1),
+			IPRange{netip.AddrFrom4([4]byte{1, 0, 0, 0}), netip.AddrFrom4([4]byte{2, 0, 0, 0})},
+			netip.AddrFrom4([4]byte{0, 0, 0, 1}),
 			false,
 		},
 	}
@@ -101,8 +102,8 @@ func TestIPRangeFromCIDR(t *testing.T) {
 		{
 			"simple, no bits",
 			IPRange{
-				Start: net.IPv4(192, 168, 25, 0),
-				End:   net.IPv4(192, 168, 25, 255),
+				Start: netip.AddrFrom4([4]byte{192, 168, 25, 0}),
+				End:   netip.AddrFrom4([4]byte{192, 168, 25, 255}),
 			},
 
 			"192.168.25.0/24",
@@ -110,32 +111,32 @@ func TestIPRangeFromCIDR(t *testing.T) {
 		{
 			"one high bit masked off",
 			IPRange{
-				Start: net.IPv4(192, 168, 25, 0),
-				End:   net.IPv4(192, 168, 25, 127),
+				Start: netip.AddrFrom4([4]byte{192, 168, 25, 0}),
+				End:   netip.AddrFrom4([4]byte{192, 168, 25, 127}),
 			},
 			"192.168.25.0/25",
 		},
 		{
 			"two high bits masked off",
 			IPRange{
-				Start: net.IPv4(192, 168, 25, 0),
-				End:   net.IPv4(192, 168, 25, 63),
+				Start: netip.AddrFrom4([4]byte{192, 168, 25, 0}),
+				End:   netip.AddrFrom4([4]byte{192, 168, 25, 63}),
 			},
 			"192.168.25.0/26",
 		},
 		{
 			"only low bit allowed",
 			IPRange{
-				Start: net.IPv4(192, 168, 25, 0),
-				End:   net.IPv4(192, 168, 25, 1),
+				Start: netip.AddrFrom4([4]byte{192, 168, 25, 0}),
+				End:   netip.AddrFrom4([4]byte{192, 168, 25, 1}),
 			},
 			"192.168.25.0/31",
 		},
 		{
 			"all bits masked",
 			IPRange{
-				Start: net.IPv4(192, 168, 25, 0),
-				End:   net.IPv4(192, 168, 25, 0),
+				Start: netip.AddrFrom4([4]byte{192, 168, 25, 0}),
+				End:   netip.AddrFrom4([4]byte{192, 168, 25, 0}),
 			},
 			"192.168.25.0/32",
 		},
@@ -143,14 +144,12 @@ func TestIPRangeFromCIDR(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, network, _ := net.ParseCIDR(tt.network)
-			netRange := NetToRange(network)
-			// We are using True() instead of Equal() because the assert
-			// library specifically tests for byteslices (which net.IP
-			// is), and tests them differently.
-			assert.True(t, tt.iprange.Start.Equal(netRange.Start),
+			network, _ := netip.ParsePrefix(tt.network)
+			netRange, _ := NetToRange(network)
+
+			assert.True(t, tt.iprange.Start.Compare(netRange.Start) == 0,
 				"net range starts: %s (expected) should equal %s", tt.iprange.Start, netRange.Start)
-			assert.True(t, tt.iprange.End.Equal(netRange.End),
+			assert.True(t, tt.iprange.End.Compare(netRange.End) == 0,
 				"net range ends: %s (expected) should equal %s", tt.iprange.End, netRange.End)
 		})
 
