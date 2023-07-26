4decfa24f863b6c6a899bac5bbfb4e492499e285
Preetham Thava <pthava@arista.com>
2023-07-26 14:50:11 -0600

    renaming - review comments

--- services/settings/dynamic_lists/types.go
+++ services/settings/dynamic_lists/types.go
@@ -2,8 +2,8 @@ package dynamic_lists
 
 // Example for this struct is present on mfw_schema
 
-// DynamicListsConfig is the data structure for JSON marshalling and unamrshalling Dynamic Lists configurations.
-type DynamicListsConfig struct {
+// Configs is the data structure for JSON marshalling and unamrshalling Dynamic Lists configurations under dynamic_lists package
+type Config struct {
 	Name           string   `json:"name"`
 	ID             string   `json:"id"`
 	Type           string   `json:"type"`
