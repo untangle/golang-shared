540a16aa66efdbebda9fe0213a3a90352895a3f0
Amit Mishra <amit.mishra@arista.com>
2023-07-27 16:46:10 +0530

    [MFW-3285] Rename files name

--- structs/protocolbuffers/CaptivePortalEvent/captiveportal_grpc.pb.go
+++ structs/protocolbuffers/CaptivePortalEvent/captiveportal_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.3.0
 // - protoc             v3.21.12
-// source: PolicyEvent.proto
+// source: captiveportal.proto
 
 package CaptivePortalEvent
 
@@ -142,5 +142,5 @@ var CaptiveUserEvent_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "PolicyEvent.proto",
+	Metadata: "captiveportal.proto",
 }
