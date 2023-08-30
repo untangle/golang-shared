de0765b34250ae59e0c3c7bbaf05572242539599
Manuwela Kanade <manuwela.kanade@gmail.com>
2023-08-30 14:43:34 +0530

    MFW-3437 Adding rules datastructures to captive portal

---
+++ services/captiveportal/captiveportal.go
@@ -0,0 +1,44 @@
+package captiveportal
+
+type CpRulesConditions struct {
+	Op    string `json:"op"`
+	Type  string `json:"type"`
+	Value string `json:"value"`
+	// CompareValue is used when evaluating conditions,
+	// the Value is compared against this field using Op operator.
+	CompareValue any `json:"-"`
+}
+
+// CustomRulesAction
+type CpRulesAction string
+
+const (
+	CpRulesEnable  CpRulesAction = "enable"
+	CpRulesDisable CpRulesAction = "disable"
+)
+
+// Captive portal rules
+type CpRules struct {
+	RuleId      string               `json:"rule_id"`
+	Enabled     bool                 `json:"enabled"`
+	Description string               `json:"description"`
+	Conditions  []*CpRulesConditions `json:"conditions"`
+	Action      CpRulesAction
+}
+
+// captive-portal user settings
+type CpSettingType struct {
+	Enabled          bool   `json:"enabled"`
+	TimeoutValue     int16  `json:"timeoutValue"`
+	TimeoutPeriod    string `json:"timeoutPeriod"`
+	AcceptText       string `json:"acceptText"`
+	AcceptButtonText string `json:"acceptButtonText"`
+	MessageText      string `json:"messageText"`
+	TosText          string `json:"tosText"`
+	WelcomeText      string `json:"welcomeText"`
+	Base64ImageData  struct {
+		EncodedBase64Image string `json:"imageData"`
+		ImageName          string `json:"imageName"`
+	} `json:"logo"`
+	Rules []*CpRules `json:"rules"`
+}
