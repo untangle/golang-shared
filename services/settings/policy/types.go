c198996140a1ed172d7134f3342aa29639fe84d2
John Sommerville <jsommerville@arista.com>
2023-09-11 13:23:59 -0600

    types: add Rules json to Policy struct

--- services/settings/policy/types.go
+++ services/settings/policy/types.go
@@ -256,11 +256,14 @@ func (pConfig *PolicyConfiguration) UnmarshalJSON(data []byte) error {
 
 // Policies are the root of our policy configurations. It includes pointers to substructure.
 type Policy struct {
-	Defaults       bool     `json:"defaults"`
-	ID             string   `json:"id"`
-	Name           string   `json:"name"`
-	Description    string   `json:"description"`
-	Enabled        bool     `json:"enabled"`
+	Defaults    bool     `json:"defaults"`
+	ID          string   `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Enabled     bool     `json:"enabled"`
+	Rules       []string `json:"rules"`
+
+	// DEPRECATED
 	Configurations []string `json:"configurations"`
 	Flows          []string `json:"flows"`
 }
