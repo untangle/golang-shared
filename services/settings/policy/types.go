6dc5d9bae21b0a462f0eb19d85ef3147f8f59572
John Sommerville <jsommerville@arista.com>
2023-09-12 11:13:06 -0600

    policy condition: move type and struct methods to common file

--- services/settings/policy/types.go
+++ services/settings/policy/types.go
@@ -3,8 +3,6 @@ package policy
 import (
 	"encoding/json"
 	"fmt"
-	"net"
-	"strconv"
 
 	utilNet "github.com/untangle/golang-shared/util/net"
 )
@@ -131,53 +129,6 @@ type PolicyFlow struct {
 	Name        string             `json:"name"`
 	Description string             `json:"description"`
 	Conditions  []*PolicyCondition `json:"conditions"`
-}
-
-// PolicyCondition contains policy condition configuration.
-type PolicyCondition struct {
-	Op      string   `json:"op"`
-	CType   string   `json:"type"`
-	Value   []string `json:"value,omitempty"`
-	GroupID string   `json:"groupId"`
-}
-
-// Unmarshal policy condition so that types of values can be checked
-func (pCondition *PolicyCondition) UnmarshalJSON(data []byte) error {
-	// unmarshal like normal first
-	type aliasPolicyCondition PolicyCondition
-	if err := json.Unmarshal(data, (*aliasPolicyCondition)(pCondition)); err != nil {
-		return err
-	}
-
-	// check that pCondition.Value is formatted correctly for the CType
-	for i, value := range pCondition.Value {
-		switch pCondition.CType {
-		case "CLIENT_ADDRESS", "SERVER_ADDRESS":
-			// Check that address is in CIDR format (w/ mask)
-			if _, _, err := net.ParseCIDR(value); err != nil {
-				// If address is a valid IP, but without a mask, just add the default
-				if ip := net.ParseIP(value); ip != nil {
-					if ip.To4() != nil {
-						pCondition.Value[i] = fmt.Sprintf("%s%s", value, "/32")
-					} else {
-						pCondition.Value[i] = fmt.Sprintf("%s%s", value, "/64")
-					}
-				} else {
-					return fmt.Errorf("error while unmarshalling policy condition: value does not match type (%s) due to error (%v)", pCondition.CType, err)
-				}
-			}
-		case "CLIENT_PORT", "SERVER_PORT":
-			if _, err := strconv.ParseUint(value, 10, 32); err != nil {
-				return fmt.Errorf("error while unmarshalling policy condition: value does not match type (%s) due to error (%v)", pCondition.CType, err)
-			}
-		case "DAY_OF_WEEK", "DEST_ADDRESS", "GEOIP_LOCATION", "INTERFACE", "SERVICE_ENDPOINT", "SOURCE_ADDRESS", "PROTOCOL_TYPE", "TIME_OF_DAY", "VLAN_ID", "THREATPREVENTION":
-			// These are not yet implemented and need to have a designated format
-		default:
-			return fmt.Errorf("error while unmarshalling policy condition: invalid type: %s", pCondition.CType)
-		}
-	}
-
-	return nil
 }
 
 // PolicyConfiguration contains policy configuration.
