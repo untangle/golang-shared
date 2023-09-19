6481a48181f86c783b5ad73909c412a13c63934f
John Sommerville <jsommerville@arista.com>
2023-09-19 08:31:21 -0600

    policy settings: clean up some functions and fix FindConfigsWithEnabled for new configuration formats

--- services/settings/policy/policy_settings.go
+++ services/settings/policy/policy_settings.go
@@ -1,8 +1,6 @@
 package policy
 
 import (
-	"fmt"
-
 	logService "github.com/untangle/golang-shared/services/logger"
 	"github.com/untangle/golang-shared/services/settings"
 )
@@ -36,9 +34,9 @@ type PolicySettings struct {
 }
 
 // FindConfiguration searches this PolicySetting to load a configuration by ID
-func (p *PolicySettings) FindConfiguration(c string) *PolicyConfiguration {
+func (p *PolicySettings) FindConfiguration(configID string) *PolicyConfiguration {
 	for _, config := range p.Configurations {
-		if config.ID == c {
+		if config.ID == configID {
 			return config
 		}
 	}
@@ -46,9 +44,9 @@ func (p *PolicySettings) FindConfiguration(c string) *PolicyConfiguration {
 }
 
 // FindRule searches this PolicySetting to load a rule by ID
-func (p *PolicySettings) FindRule(c string) *Object {
+func (p *PolicySettings) FindRule(ruleID string) *Object {
 	for _, rule := range p.Rules {
-		if rule.ID == c {
+		if rule.ID == ruleID {
 			return rule
 		}
 	}
@@ -65,20 +63,20 @@ func (p *PolicySettings) FindFlow(id string) *PolicyFlow {
 	return nil
 }
 
-// FindConfigsWithEnabled returns the configs with enabled status.
+// FindConfigsWithEnabled returns the names of configs with enabled status that are attached to this policy
 func (p *PolicySettings) FindConfigsWithEnabled(pol *Policy, enabled bool) []string {
-	configs := []string{}
-	for _, configID := range pol.Configurations {
-		config := p.findConfiguration(configID)
-		if config != nil && config.AppSettings != nil {
-			for pluginName, pluginSettings := range config.AppSettings {
-				if pluginSettings.(map[string]interface{})["enabled"] == enabled {
-					configs = append(configs, pluginName)
-				}
+	configNames := []string{}
+	for _, ruleID := range pol.Rules {
+		rule := p.FindRule(ruleID)
+		if rule != nil {
+			// We also have to check if this config is enabled/disabled...
+			configDetails := p.FindConfiguration(rule.Action.UUID)
+			if configDetails.Enabled == enabled {
+				configNames = append(configNames, rule.Action.Key)
 			}
 		}
 	}
-	return configs
+	return configNames
 }
 
 // Returns a list of disabled app services for a given policy ID.
