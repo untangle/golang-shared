b0272940cb40115bbd5442f4bb495b988d440a0e
Naveen Gowda <naveen.gowda@arista.com>
2023-10-06 03:24:51 -0700

    3456 Golang changes

--- services/settings/quota/types_quota.go
+++ services/settings/quota/types_quota.go
@@ -1,7 +1,7 @@
 package quota
 
 import (
-	"time"
+    "time"
 )
 
 type QuotaConditions struct {
@@ -12,18 +12,18 @@ type QuotaConditions struct {
 }
 
 type QuotaExceedActions struct {
-   ID				string		`json:"id"`
-   Name			string		`json:name`
-   Description	string		`json:description`
-   Action		string		`json:action`
-   Priority		string		`json:priroity`
-   Default		bool			`json:enabled`
+	ID          string        `json:"id"`
+	Name        string        `json:name`
+	Description string        `json:description`
+	Action      string        `json:action`
+	Priority    string        `json:priroity`
+	Default     bool          `json:enabled`
 }
 
 type QuotaAction struct {
-   Type				string	`json:"type"`
-   QuotaID			string	`json:"quota_id"`
-   ExceedActionID	string	`json:"exceed_action_id"`
+	Type             string    `json:"type"`
+	QuotaID          string    `json:"quota_id"`
+	ExceedActionID   string    `json:"exceed_action_id"`
 }
 
 type QuotaConfiguration struct {
@@ -41,18 +41,18 @@ type QuotaRules struct {
 	Conditions  []*QuotaConditions `json:"conditions"`
 	Actions     *QuotaAction       `json:"action"`
 
-	Command		string
-	MatchCmd		string
-	Timer			*time.Ticker
-	ChSignal    <-chan bool
+	Command     string
+	MatchCmd    string
+	Timer       *time.Ticker
+	ChSignal    chan bool
 }
 
 // QuotaSettings is the main data structure for Quota Management.
 // It contains an array of QuotaConfigurations, an array of QuotaRules and an array
 // of exceed_actions.
 type QuotaSettings struct {
-	Enabled			bool							`json:"enabled"`
-	Configurations	[]*QuotaConfiguration	`json:"configuration"`
-	Rules				[]*QuotaRules				`json:"rules"`
-	ExceedActions	[]*QuotaExceedActions	`json:"exceed_actions"`
+	Enabled           bool                     `json:"enabled"`
+	Configurations    []*QuotaConfiguration    `json:"configuration"`
+	Rules             []*QuotaRules            `json:"rules"`
+	ExceedActions     []*QuotaExceedActions    `json:"exceed_actions"`
 }
