7179412d6911d5ea095d8b323869ab53bc0af77d
Al Parker <al.parker@arista.com>
2023-10-11 16:00:50 -0400

    MFW-3695 Reorganizing

--- services/settings/policy/condition.go
+++ services/settings/policy/condition.go
@@ -48,42 +48,7 @@ func (pCondition *PolicyCondition) UnmarshalJSON(data []byte) error {
 		return err
 	}
 	// Check to see if this is Conditions is using an ApplicationObject or ...Group
-	if pCondition.CType != "CLIENT" && pCondition.CType != "SERVER" {
-
-		// Not using an ApplicationObject
-		if pCondition.Op != "match" && pCondition.Op != "in" {
-			// check that pCondition.Value is formatted correctly for the CType
-			for i, value := range pCondition.Value {
-				switch pCondition.CType {
-				case "CLIENT_ADDRESS", "SERVER_ADDRESS", "SOURCE_ADDRESS", "DESTINATION_ADDRESS":
-					// Check that address is in CIDR format (w/ mask)
-					if _, _, err := net.ParseCIDR(value); err != nil {
-						// If address is a valid IP, but without a mask, just add the default
-						if ip := net.ParseIP(value); ip != nil {
-							if ip.To4() != nil {
-								pCondition.Value[i] = fmt.Sprintf("%s%s", value, "/32")
-							} else {
-								pCondition.Value[i] = fmt.Sprintf("%s%s", value, "/128")
-							}
-						} else {
-							return fmt.Errorf("error while unmarshalling policy condition: value does not match type (%s) due to error (%v)", pCondition.CType, err)
-						}
-					}
-				case "CLIENT_PORT", "SERVER_PORT":
-					if _, err := strconv.ParseUint(value, 10, 32); err != nil {
-						return fmt.Errorf("error while unmarshalling policy condition: value does not match type (%s) due to error (%v)", pCondition.CType, err)
-					}
-				// just string type values on these, no need to validate
-				case "DAY_OF_WEEK", "GEOIP_LOCATION", "INTERFACE", "SERVICE_ENDPOINT",
-					"PROTOCOL_TYPE", "TIME_OF_DAY", "VLAN_ID", "THREATPREVENTION":
-
-				default:
-					return fmt.Errorf("error while unmarshalling policy condition: invalid type: %s", pCondition.CType)
-				}
-			}
-		}
-	} else {
-		// p.CType is CLIENT or SERVER
+	if pCondition.CType == "CLIENT" || pCondition.CType == "SERVER" {
 		// Using an ApplicationObject or ...Group
 		// Try for an ApplicationObjecGroup with Items first
 		appObjectGroup := ApplicationObjectGroup{}
@@ -101,6 +66,36 @@ func (pCondition *PolicyCondition) UnmarshalJSON(data []byte) error {
 			}
 		}
 		pCondition.Optional = appObjectGroup
+	} else if pCondition.Op != "match" && pCondition.Op != "in" {
+		// check that pCondition.Value is formatted correctly for the CType
+		for i, value := range pCondition.Value {
+			switch pCondition.CType {
+			case "CLIENT_ADDRESS", "SERVER_ADDRESS", "SOURCE_ADDRESS", "DESTINATION_ADDRESS":
+				// Check that address is in CIDR format (w/ mask)
+				if _, _, err := net.ParseCIDR(value); err != nil {
+					// If address is a valid IP, but without a mask, just add the default
+					if ip := net.ParseIP(value); ip != nil {
+						if ip.To4() != nil {
+							pCondition.Value[i] = fmt.Sprintf("%s%s", value, "/32")
+						} else {
+							pCondition.Value[i] = fmt.Sprintf("%s%s", value, "/128")
+						}
+					} else {
+						return fmt.Errorf("error while unmarshalling policy condition: value does not match type (%s) due to error (%v)", pCondition.CType, err)
+					}
+				}
+			case "CLIENT_PORT", "SERVER_PORT":
+				if _, err := strconv.ParseUint(value, 10, 32); err != nil {
+					return fmt.Errorf("error while unmarshalling policy condition: value does not match type (%s) due to error (%v)", pCondition.CType, err)
+				}
+			// just string type values on these, no need to validate
+			case "DAY_OF_WEEK", "GEOIP_LOCATION", "INTERFACE", "SERVICE_ENDPOINT",
+				"PROTOCOL_TYPE", "TIME_OF_DAY", "VLAN_ID", "THREATPREVENTION":
+
+			default:
+				return fmt.Errorf("error while unmarshalling policy condition: invalid type: %s", pCondition.CType)
+			}
+		}
 	}
 	return nil
 }
