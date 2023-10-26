4619644c5f7ba49ad0fce9bd5c2a9908d0e153ff
vishalmane09 <vishal.mane-ext@arista.com>
2023-10-26 22:40:24 +0530

    MFW-3413 Capiltalized a variable names

--- services/settings/policy/object.go
+++ services/settings/policy/object.go
@@ -83,7 +83,7 @@ func (obj *Object) UnmarshalJSON(data []byte) error {
 		// drop down to the default return
 
 	case ApplicationControlRuleObject, CaptivePortalRuleObject, GeoipRuleObject, ThreatPreventionRuleObject,
-		NATRuleObject, PortForwardRuleObject, SecurityRuleObject, ShapingRuleObject, WANPolicyRuleObject, webfilterRuleObject:
+		NATRuleObject, PortForwardRuleObject, SecurityRuleObject, ShapingRuleObject, WANPolicyRuleObject, WebFilterRuleObject:
 		// drop down to the default return
 
 	case GeoipConfigType, WebFilterConfigType, ThreatPreventionConfigType,
