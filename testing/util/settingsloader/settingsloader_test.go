ae0e07c5e41a3c90b8f53b70f830910c85bdc118
John Phillips <jphillips@arista.com>
2023-10-31 16:11:35 -0600

    Update for damn linter.

--- testing/util/settingsloader/settingsloader_test.go
+++ testing/util/settingsloader/settingsloader_test.go
@@ -2,25 +2,23 @@ package settingsloader
 
 import (
 	"testing"
-
-	"github.com/untangle/golang-shared/services/settings/policy"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestPolicyManagerSettingsFile(t *testing.T) {
+	t.Skip("broken by upstream schema.")
+	// commented because the linter doesn't like this.
 
-	settingskey := []string{"policy_manager"}
+	// settingskey := []string{"policy_manager"}
 
-	t.Run("load_global_settings_file", func(t *testing.T) {
-		result := policy.PolicySettings{}
-		err := LoadSettingsFromURL(&result, settingskey,
-			"https://raw.githubusercontent.com/untangle/mfw_schema/master/v1/policy_manager/test_settings.json",
-		)
-		t.Skip("broken by upstream schema.")
-		assert.NoError(t, err)
+	// t.Run("load_global_settings_file", func(t *testing.T) {
+	// 	result := policy.PolicySettings{}
+	// 	err := LoadSettingsFromURL(&result, settingskey,
+	// 		"https://raw.githubusercontent.com/untangle/mfw_schema/master/v1/policy_manager/test_settings.json",
+	// 	)
 
-		// This may change, when the example settings file changes.
-		assert.Equal(t, len(result.Rules), 13)
-	})
+	// 	assert.NoError(t, err)
+
+	// 	// This may change, when the example settings file changes.
+	// 	assert.Equal(t, len(result.Rules), 13)
+	// })
 }
