9cc6e4065c2bd69f94ac18089bffe683fd1b8abd
John Phillips <jphillips@arista.com>
2023-10-31 16:03:54 -0600

    Fix linter problem.

--- testing/util/settingsloader/settingsloader_test.go
+++ testing/util/settingsloader/settingsloader_test.go
@@ -11,13 +11,13 @@ import (
 func TestPolicyManagerSettingsFile(t *testing.T) {
 
 	settingskey := []string{"policy_manager"}
-	t.Skip("broken by upstream schema.")
+
 	t.Run("load_global_settings_file", func(t *testing.T) {
 		result := policy.PolicySettings{}
 		err := LoadSettingsFromURL(&result, settingskey,
 			"https://raw.githubusercontent.com/untangle/mfw_schema/master/v1/policy_manager/test_settings.json",
 		)
-
+		t.Skip("broken by upstream schema.")
 		assert.NoError(t, err)
 
 		// This may change, when the example settings file changes.
