010647ac04a18438bd2b3d8e6b14e39b98684225
John Phillips <jphillips@arista.com>
2023-11-01 10:46:26 -0600

    Comment out test for damn linter.

--- testing/util/settingsloader/settingsloader_test.go
+++ testing/util/settingsloader/settingsloader_test.go
@@ -5,13 +5,15 @@ import (
 )
 
 func TestPolicyManagerSettingsFile(t *testing.T) {
-	settingskey := []string{"policy_manager"}
-	t.Run("load_global_settings_file", func(t *testing.T) {
-		// Skip test, until port ranges have been added to loader. See MFW-3775
-		t.Skip("Skipping test until tasks listed in MFW-3775 are completed")
-		result := policy.PolicySettings{}
-		err := LoadSettingsFromURL(&result, settingskey,
-			"https://raw.githubusercontent.com/untangle/mfw_schema/master/v1/policy_manager/test_settings.json",
-		)
-	}
+	// Skip test, until port ranges have been added to loader. See MFW-3775
+	t.Skip("Skipping test until tasks listed in MFW-3775 are completed")
+
+	// settingskey := []string{"policy_manager"}
+	// t.Run("load_global_settings_file", func(t *testing.T) {
+
+	// 	result := policy.PolicySettings{}
+	// 	err := LoadSettingsFromURL(&result, settingskey,
+	// 		"https://raw.githubusercontent.com/untangle/mfw_schema/master/v1/policy_manager/test_settings.json",
+	// 	)
+	// }
 }
