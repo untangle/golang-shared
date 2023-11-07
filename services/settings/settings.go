bdf0e5c26c3652afba33913c2c564430f2ffc46f
rahulg <rahulg@arista.com>
2023-11-07 02:21:06 -0800

    Sighup changes on top of v1.35.0

--- services/settings/settings.go
+++ services/settings/settings.go
@@ -504,6 +504,12 @@ func syncAndSave(jsonObject map[string]interface{}, filename string, force bool)
 				logger.Info("Discovery is not enabled hence skipping sighup\n")
 				continue
 			}
+			_, err := os.Stat("/usr/bin/updateSysdbSignal")
+			if err == nil || !os.IsNotExist(err) {
+				if err := exec.Command("/usr/bin/updateSysdbSignal", "--sighup").Run(); err != nil {
+					logger.Warn("Failed to run EOS-MFW script `updateSysdbSignal` command with error: %+v\n", err)
+				}
+			}
 			err = util.RunSighup(executable)
 			if err != nil {
 				logger.Warn("Failure running sighup on required executable %s: %s\n", executable, err.Error())
