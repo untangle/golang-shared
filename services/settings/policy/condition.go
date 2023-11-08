263bb60b4765e69391ef6ab43a418aa6e1e34458
Indira Ramasamy <iramasamy@arista.com>
2023-11-08 10:10:22 -0700

    MFW-3776: Changed Client and server application type to match CV

--- services/settings/policy/condition.go
+++ services/settings/policy/condition.go
@@ -49,7 +49,7 @@ func (pCondition *PolicyCondition) UnmarshalJSON(data []byte) error {
 			// just string type values on these, no need to validate
 			case "DAY_OF_WEEK", "GEOIP_LOCATION", "INTERFACE", "SERVICE_ENDPOINT", "SERVICE_ENDPOINT_SERVER", "SERVICE_ENDPOINT_CLIENT",
 				"PROTOCOL_TYPE", "APPLICATION_CATEGORY", "TIME_OF_DAY", "VLAN_ID", "THREATPREVENTION",
-				"APPLICATION", "SERVER_APPLICATION", "CLINET_APPLICATION":
+				"APPLICATION", "SERVER_APPLICATION", "CLIENT_APPLICATION":
 
 			default:
 				return fmt.Errorf("error while unmarshalling policy condition: invalid type: %s", pCondition.CType)
