6634eb08bd4327e3c1e3032fd59dd8bfeb6fcafa
Christian Rasmussen <crasmussen@arista.com>
2023-11-11 10:44:43 -0700

    Feedback and add WANPolicy type

--- services/settings/policy/wans.go
+++ services/settings/policy/wans.go
@@ -1,12 +1,17 @@
 package policy
 
+// WANCriteriaType is a type of WAN criteria
 type WANCriteriaType struct {
 	Type string `json:"type"`
 }
 
+// WANInterfaceType is a type of WAN interface
 type WANInterfaceType struct {
 	ID uint `json:"interfaceId"`
 }
+
+// WANPolicy is an object with Type Object
+type WANPolicy Object
 
 // WANPolicySettings are settings for a WAN policy.
 type WANPolicySettings struct {
