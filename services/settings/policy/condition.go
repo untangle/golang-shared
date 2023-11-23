435f762b3a42de6ca1160edd960c8689d6b999ee
Al Parker <al.parker@arista.com>
2023-11-23 15:18:08 -0500

    MFW-3942 Adding certificate condition types (#310)

--- services/settings/policy/condition.go
+++ services/settings/policy/condition.go
@@ -47,7 +47,8 @@ func (pCondition *PolicyCondition) UnmarshalJSON(data []byte) error {
 					return fmt.Errorf("error while unmarshalling policy condition: value does not match type (%s) due to error (%v)", pCondition.CType, err)
 				}
 			// just string type values on these, no need to validate
-			case "DAY_OF_WEEK", "SERVER_GEOIP", "CLIENT_GEOIP", "INTERFACE", "SERVICE", "SERVER_SERVICE", "CLIENT_SERVICE",
+			case "CERT_SUBJECT_COMMON_NAME", "CERT_SUBJECT_ORGANIZATION",
+				"DAY_OF_WEEK", "SERVER_GEOIP", "CLIENT_GEOIP", "INTERFACE", "SERVICE", "SERVER_SERVICE", "CLIENT_SERVICE",
 				"PROTOCOL_TYPE", "APPLICATION_CATEGORY", "TIME_OF_DAY", "VLAN_TAG", "THREATPREVENTION",
 				"APPLICATION", "SERVER_APPLICATION", "CLIENT_APPLICATION", "HOSTNAME":
 
