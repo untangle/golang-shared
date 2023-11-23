7d6e6b2cb39c268ced3c38ee15b5204835809db2
sudhan <sudhan@arista.com>
2023-11-22 20:28:19 -0800

    Addressed review comments

--- services/settings/settings.go
+++ services/settings/settings.go
@@ -85,8 +85,8 @@ func GetSettings(segments []string) (interface{}, error) {
 }
 
 // SetSettings updates the settings
-func SetSettings(segments []string, value interface{}, force bool, skip bool) (interface{}, error) {
-	return SetSettingsFile(segments, value, settingsFile, force, skip)
+func SetSettings(segments []string, value interface{}, force bool, skipEosConfig bool) (interface{}, error) {
+	return SetSettingsFile(segments, value, settingsFile, force, skipEosConfig)
 }
 
 // TrimSettings trims the settings
@@ -136,7 +136,7 @@ func GetSettingsFile(segments []string, filename string) (interface{}, error) {
 }
 
 // SetSettingsFile updates the settings
-func SetSettingsFile(segments []string, value interface{}, filename string, force bool, skip bool) (interface{}, error) {
+func SetSettingsFile(segments []string, value interface{}, filename string, force bool, skipEosConfig bool) (interface{}, error) {
 	var ok bool
 	var err error
 	var jsonSettings map[string]interface{}
@@ -158,7 +158,7 @@ func SetSettingsFile(segments []string, value interface{}, filename string, forc
 	}
 
 	saveLocker.Lock()
-	output, err := syncAndSave(jsonSettings, filename, force, skip)
+	output, err := syncAndSave(jsonSettings, filename, force, skipEosConfig)
 	saveLocker.Unlock()
 	if err != nil {
 		var errJSON map[string]interface{}
@@ -416,9 +416,11 @@ func getSettingsFromJSON(jsonObject interface{}, segments []string) (interface{}
 }
 
 // runSyncSettings runs sync-settings on the specified filename
-func runSyncSettings(filename string, force bool, skip bool) (string, error) {
+func runSyncSettings(filename string, force bool, skipEosConfigEosConfig bool) (string, error) {
 
-	cmd := exec.Command("/usr/bin/sync-settings", "-f", filename, "-v", "force="+strconv.FormatBool(force), "-v", "skip="+strconv.FormatBool(skip))
+	//If skipEosConfig is set, we will skip load-eos-config while doing sync-settings.
+	//This flag will be set only when POST calls are made from SuperServer plugin in MFW.
+	cmd := exec.Command("/usr/bin/sync-settings", "-f", filename, "-v", "force="+strconv.FormatBool(force), "-v", "skipEosConfig="+strconv.FormatBool(skipEosConfig))
 	outBytes, err := cmd.CombinedOutput()
 	jsonOutput, data, errParse := parseSyncSettingsJsonOutput(outBytes)
 
@@ -474,7 +476,7 @@ func syncSystemFiles() {
 // it copies the tmp file to the destination specified in filename
 // if sync-settings does not succeed it returns the error and output
 // returns stdout, stderr, and an error
-func syncAndSave(jsonObject map[string]interface{}, filename string, force bool, skip bool) (string, error) {
+func syncAndSave(jsonObject map[string]interface{}, filename string, force bool, skipEosConfig bool) (string, error) {
 	// we want this to run after all files have been closed
 	// so we defer it as soon as possible
 	defer syncSystemFiles()
@@ -493,7 +495,7 @@ func syncAndSave(jsonObject map[string]interface{}, filename string, force bool,
 		return "Failed to write settings.", err
 	}
 
-	output, err := runSyncSettings(tmpfile.Name(), force, skip)
+	output, err := runSyncSettings(tmpfile.Name(), force, skipEosConfig)
 	scanner := bufio.NewScanner(strings.NewReader(output))
 	for scanner.Scan() {
 		logger.Info("sync-settings: %v\n", scanner.Text())
