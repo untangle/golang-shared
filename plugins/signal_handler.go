31f35f927ca9cdb8cc0dc8e91e05ab28d84e8efe
PranavAdityaCP <pranav.cp@arista.com>
2023-12-19 02:19:37 -0800

    Instrumentation

--- plugins/signal_handler.go
+++ plugins/signal_handler.go
@@ -13,6 +13,7 @@ type SignalHandler struct {
 }
 
 func NewSignalHandler() *SignalHandler {
+	logger.Warn("--------In new SignalHandler----")
 	return &SignalHandler{}
 }
 
@@ -22,13 +23,19 @@ func (handler *SignalHandler) RegisterPlugin(plugin SignalHandlingPlugin) {
 
 // Signal calls the Signal() method of all registered plugins with sig.
 func (handler *SignalHandler) Signal(sig syscall.Signal) {
+	logger.Warn("--------In sending Signal----")
 	for _, sigHandler := range handler.plugins {
+		logger.Warn("--------In FORLOOP for sif : %v ----", sig)
 		if err := sigHandler.Signal(sig); err != nil {
+			logger.Warn("--------In ERROR 1 for sig : %v ----", sig)
 			plugin := sigHandler.(Plugin)
+			logger.Warn("--------In ERROR 2 for sig : %v ----", sig)
 			logger.Warn("Plugin %s returned error handling signal %v: %s\n",
 				plugin.Name(),
 				sig,
 				err)
+			logger.Warn("--------In ERROR 3 for sig : %v ----", sig)
 		}
 	}
+	logger.Warn("--------Out sending Signal----")
 }
