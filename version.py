7bf746fb4f2eacaa9e64c50305d012871991ff92
Liviu Nicoara <lnicoara@arista.com>
2024-01-31 17:25:17 +0200

    added branch parameter

--- version.py
+++ version.py
@@ -29,23 +29,45 @@ def tag2version(tag: str):
     return None
 
 
-def find_latest_tag(fetch: bool):
+def find_latest_tag(fetch: bool, branch: str = None):
     """
     find the most recent/highest numbered tag. If fetch is True, run
     git fetch origin --tags first so we know what the tags are.
+    If branch is provided, it will get the most recent tag number available 
+    on the specified branch.
     """
     if fetch:
         subprocess.run(['git', 'fetch', 'origin', '--tags'], check=True)
+    
+    cmd = ['git', 'tag']
+    if branch != None:
+        cmd.append(f"--merged=origin/{branch}")
+    
+    process = subprocess.run(cmd, 
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.PIPE,
+                            text=True)
+    if process.returncode != 0:
+        print(
+            f'An error occured while getting tags. Command: {cmd}',
+            process.stderr)
+        raise SystemExit(1)
+    
     tags = [tag2version(i) for i in
-            (subprocess.run(['git', 'tag'], stdout=subprocess.PIPE,
-                            check=True)
-             .stdout.decode().splitlines())]
+            (process.stdout.decode().splitlines())]
+    
     latest = sorted([i for i in tags if i])[-1]
     return latest
 
 
 do_fetch = '--fetch' in sys.argv[1:]
-latest = find_latest_tag(do_fetch)
+branch = None
+if "--branch" in sys.argv[1:]:
+    branch_index = sys.argv.index("--branch") + 1
+    if branch_index < len(sys.argv):
+        branch = sys.argv[branch_index]
+        
+latest = find_latest_tag(do_fetch, branch)
 
 # Now, loop through each line of the commit message, looking for what
 # version.
