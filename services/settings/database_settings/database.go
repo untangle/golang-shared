719867cd6c7089a6675e8e9528a55849ad2a9ff9
Manuwela Kanade <manuwela.kanade@gmail.com>
2024-04-28 23:33:14 +0530

    MFW-4883 Fixing database password attr name

--- services/settings/database_settings/database.go
+++ services/settings/database_settings/database.go
@@ -1,17 +1,17 @@
 package database_settings
 
 type Database struct {
-	Enabled          bool   `json:"enabled"`
-	ID               string `json:"id"`
-	Database         string `json:"db_name"`
-	UserName         string `json:"db_username"`
-	Password         string `json:"db_password"`
-	PasswordEncoded  string `json:"db_password_encoded"`
-	Server           string `json:"db_server"`
-	Port             int    `json:"db_port"`
-	Description      string `json:"description"`
-	Name             string `json:"name"`
-	Type             string `json:"type"`
-	ConnectionString string `json:"db_connection_string"`
-	Default          bool   `json:"default"`
+	Enabled           bool   `json:"enabled"`
+	ID                string `json:"id"`
+	Database          string `json:"db_name"`
+	UserName          string `json:"db_username"`
+	Password          string `json:"db_password"`
+	PasswordEncrypted string `json:"db_password_encrypted"`
+	Server            string `json:"db_server"`
+	Port              int    `json:"db_port"`
+	Description       string `json:"description"`
+	Name              string `json:"name"`
+	Type              string `json:"type"`
+	ConnectionString  string `json:"db_connection_string"`
+	Default           bool   `json:"default"`
 }
