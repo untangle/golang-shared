85c188f13d26a90128dcce07848dfbbe063b561d
John Phillips <jphillips@arista.com>
2024-05-13 11:31:23 -0700

    Add commentary,

--- services/settings/filesystem.go
+++ services/settings/filesystem.go
@@ -7,16 +7,28 @@ import (
 	"strings"
 )
 
+// FilenameLocator finds files on the local filesytem, allowing the
+// system to be in hybrid or non-hybid mode and concealing the
+// diffrences.
 type FilenameLocator struct {
 	fileExists func(filename string) bool
 }
 
 const (
-	hybridModeGenericPrefix  = "/mfw"
+	// prefix for generic filepaths in hybrid mode
+	hybridModeGenericPrefix = "/mfw"
+
+	// kernel forwarding mode/BST container mode path prefix.
 	kernelModeSettingsPrefix = "/etc/config"
+
+	// prefix specifically for config files in hybrid mode
 	hybridModeSettingsPrefix = "/mnt/flash/mfw-settings"
 )
 
+// FileExists returns true if we can Stat the filename. We don't
+// distinguish between various kinds of errors, but do log them, on
+// the theory that if you can't Stat the filename, for most purposes,
+// that is the same as it not existing, and isn't a common case.
 func FileExists(fname string) bool {
 	if _, err := os.Stat(fname); err != nil {
 		if !os.IsNotExist(err) {
@@ -51,6 +63,8 @@ func (f *FilenameLocator) findEOSFileName(filename string) (string, error) {
 	}
 }
 
+// LocateFile locates the input filename on the filesystem,
+// automatically translating it to hybrid mode filenames when needed.
 func (f *FilenameLocator) LocateFile(filename string) (string, error) {
 	if f.fileExists(filename) {
 		return filename, nil
@@ -63,6 +77,8 @@ var defaultLocator = &FilenameLocator{
 	fileExists: FileExists,
 }
 
+// LocateFile calls FilenameLocator.LocateFile on the default filename
+// locator.
 func LocateFile(filename string) (string, error) {
 	return defaultLocator.LocateFile(filename)
 }
