4e6cb0f77acd23d421e11f968f50057205d9cb32
avinashdige15 <avinash.dige@arista.com>
2025-02-17 15:13:26 +0530

    MFW-6300: Add mock events file to services

---
+++ services/events/mock_events.go
@@ -0,0 +1,48 @@
+package mocks
+
+import (
+	"sync"
+
+	protoAlerts "github.com/untangle/golang-shared/structs/protocolbuffers/Alerts"
+	"google.golang.org/protobuf/proto"
+)
+
+// MockEventPublisher is a test stub that keeps all the Alerts,
+// Events, last alert, last Event that were sent to it.
+type MockEventPublisher struct {
+	sync.Mutex
+	Alerts    []*protoAlerts.Alert
+	Events    []*ZmqMessage
+	LastAlert *protoAlerts.Alert
+	LastEvent *ZmqMessage
+}
+
+func (m *MockEventPublisher) Send(alert *protoAlerts.Alert) {
+	m.Lock()
+	defer m.Unlock()
+	m.LastAlert = alert
+	m.Alerts = append(m.Alerts, alert)
+}
+
+func (m *MockEventPublisher) SendZmqMessage(event *ZmqMessage) {
+	m.Lock()
+	defer m.Unlock()
+	m.LastEvent = event
+	m.Events = append(m.Events, event)
+}
+
+// GetLastAlert gets the last alert.
+func (m *MockEventPublisher) GetLastAlert() *protoAlerts.Alert {
+	m.Lock()
+	defer m.Unlock()
+	copy := proto.Clone(m.LastAlert)
+	return copy.(*protoAlerts.Alert)
+}
+
+// GetLastEvent gets the last event.
+func (m *MockEventPublisher) GetLastEvent() *ZmqMessage {
+	m.Lock()
+	defer m.Unlock()
+	copy := *m.LastEvent
+	return &copy
+}
