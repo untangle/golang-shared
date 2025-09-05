ef4c0437661cd79dbe7e9e8063af07d718451912
john-phillips-arista <106119552+john-phillips-arista@users.noreply.github.com>
2025-09-05 16:00:58 -0600

    fix-constructorwrapper-gs: fix-constructorwrapper-gs (#929)
    Fix the constructor wrapper implementation for plugins so that order doesn't matter.
    
    Previously we would immediately wrap constructors when calling `RegisterPlugin`. This created an issue where if you didn't import the constructor wrappers first, they wouldn't wrap plugins, and that caused EFW-1286.
    
    Now, constructor wrappers and plugins are both simply stored in slices until `Startup()` is called, so that import order doesn't matter.

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -34,6 +34,7 @@ type pluginInfo struct {
 	plugin      Plugin
 	metadata    []any
 	constructor PluginConstructor
+	isProvider  bool
 
 	// a function built using the reflect package that will set
 	// the plugin value of this struct. Executed during Startup().
@@ -53,9 +54,10 @@ type predicateInfo struct {
 // keeps track of a list of plugins to send method calls to.
 type PluginControl struct {
 	dig.Container
-	wrapper    ConstructorWrapper
-	predicates []*predicateInfo
-	pluginInfo []*pluginInfo
+	wrapper            ConstructorWrapper
+	wrapperInvokerFunc interface{}
+	predicates         []*predicateInfo
+	pluginInfo         []*pluginInfo
 
 	consumers          []consumer
 	enableStartupPanic bool
@@ -162,9 +164,7 @@ func (control *PluginControl) RegisterConstructorWrapper(wrapper ConstructorWrap
 			control.wrapper = wrapper
 			return []reflect.Value{}
 		})
-	if err := control.Invoke(invokerFunc.Interface()); err != nil {
-		panic(fmt.Sprintf("couldn't instantiate wrapper: %s", err))
-	}
+	control.wrapperInvokerFunc = invokerFunc.Interface()
 }
 
 // RegisterPluginPredicate registers to the 'control' object a plugin
@@ -207,38 +207,15 @@ func (control *PluginControl) RegisterPluginPredicate(predicateFactory PluginPre
 // by the DI container and returns a plugin object. This function will
 // not provide the plugin as a potential dependency for other plugins.
 func (control *PluginControl) RegisterPlugin(constructor PluginConstructor, metadata ...any) {
-	constructorType := reflect.TypeOf(constructor)
-	constructorVal := reflect.ValueOf(constructor)
-	inputs := []reflect.Type{}
-	for i := 0; i < constructorType.NumIn(); i++ {
-		inputs = append(inputs, constructorType.In(i))
-	}
-
-	if control.wrapper != nil && control.wrapper.Matches(constructor, metadata...) {
-		constructorVal = makeWrapperConstructor(control.wrapper, constructor, metadata)
-	}
-
 	pluginInfo := &pluginInfo{
 		constructor: constructor,
 		metadata:    metadata,
+		isProvider:  false,
 
 		// to be set later by our constructed function, if
 		// needed.
 		plugin: nil,
 	}
-
-	// create a func at runtime that we can invoke that calls the
-	// constructor and appends the return value to the list of plugins.
-	saverFunc := reflect.MakeFunc(
-		reflect.FuncOf(inputs, []reflect.Type{}, false),
-		func(vals []reflect.Value) []reflect.Value {
-			output := constructorVal.Call(vals)
-			plugin := output[0].Interface()
-			pluginIntf := plugin.(Plugin)
-			pluginInfo.plugin = pluginIntf
-			return []reflect.Value{}
-		})
-	pluginInfo.saverFunc = saverFunc.Interface()
 	control.pluginInfo = append(control.pluginInfo, pluginInfo)
 }
 
@@ -257,44 +234,68 @@ func (control *PluginControl) GetRegisteredPluginCount() int {
 // plugins may require it. It is not instantiated until the Startup()
 // method is called.
 func (control *PluginControl) RegisterAndProvidePlugin(constructor PluginConstructor, metadata ...any) {
-	constructorType := reflect.TypeOf(constructor)
-	outputType := constructorType.Out(0)
 	pluginInfo := &pluginInfo{
 		plugin:      nil,
 		metadata:    metadata,
 		constructor: constructor,
 		saverFunc:   nil,
+		isProvider:  true,
 	}
 	control.pluginInfo = append(control.pluginInfo, pluginInfo)
-	// create a func at runtime that we can invoke that requires the
-	// plugin to ensure it gets instantiated, and also appends it to
-	// the list of registered plugins.  This is different from in
-	// RegisterPlugin because here we require the plugin as an input,
-	// which in turn also requires the plugin to have been provided as
-	// an output via Provide() (see below).
-	//
-	// Although similar to RegisterPlugin(), this is fundamentally
-	// different and can't be merged with that implementation as that
-	// implementation must allow multiple registrations of identical
-	// types, since they may have different constructor functions.
-	saverFunc := reflect.MakeFunc(
-		reflect.FuncOf([]reflect.Type{outputType}, []reflect.Type{}, false),
-		func(vals []reflect.Value) []reflect.Value {
-			plugin := vals[0].Interface()
-			pluginIntf := plugin.(Plugin)
-			pluginInfo.plugin = pluginIntf
-			return []reflect.Value{}
-		})
-	pluginInfo.saverFunc = saverFunc.Interface()
-	if err := control.Provide(constructor); err != nil {
-		panic(fmt.Sprintf(
-			"couldn't register plugin constructor as a provider: %v, err: %s", constructor, err))
-	}
 }
 
 // UnregisterPlugin removes a plugin from the list of plugins
 func (control *PluginControl) UnregisterPluginByIndex(indx int) {
 	control.pluginInfo = append(control.pluginInfo[:indx], control.pluginInfo[indx+1:]...)
+}
+
+func (control *PluginControl) preparePlugins() {
+	for _, pluginInfo := range control.pluginInfo {
+		constructor := pluginInfo.constructor
+		constructorVal := reflect.ValueOf(constructor)
+
+		// first, see if we need to wrap the constructor.
+		if control.wrapper != nil && control.wrapper.Matches(constructor, pluginInfo.metadata...) {
+			constructorVal = makeWrapperConstructor(control.wrapper, constructor, pluginInfo.metadata)
+		}
+
+		actualConstructor := constructorVal.Interface()
+
+		if pluginInfo.isProvider {
+			constructorType := reflect.TypeOf(actualConstructor)
+			outputType := constructorType.Out(0)
+
+			saverFunc := reflect.MakeFunc(
+				reflect.FuncOf([]reflect.Type{outputType}, []reflect.Type{}, false),
+				func(vals []reflect.Value) []reflect.Value {
+					plugin := vals[0].Interface()
+					pluginIntf := plugin.(Plugin)
+					pluginInfo.plugin = pluginIntf
+					return []reflect.Value{}
+				})
+			pluginInfo.saverFunc = saverFunc.Interface()
+			if err := control.Provide(actualConstructor); err != nil {
+				panic(fmt.Sprintf(
+					"couldn't register plugin constructor as a provider: %v, err: %s", constructor, err))
+			}
+		} else {
+			constructorType := reflect.TypeOf(actualConstructor)
+			inputs := []reflect.Type{}
+			for i := 0; i < constructorType.NumIn(); i++ {
+				inputs = append(inputs, constructorType.In(i))
+			}
+			saverFunc := reflect.MakeFunc(
+				reflect.FuncOf(inputs, []reflect.Type{}, false),
+				func(vals []reflect.Value) []reflect.Value {
+					output := constructorVal.Call(vals)
+					plugin := output[0].Interface()
+					pluginIntf := plugin.(Plugin)
+					pluginInfo.plugin = pluginIntf
+					return []reflect.Value{}
+				})
+			pluginInfo.saverFunc = saverFunc.Interface()
+		}
+	}
 }
 
 // filterPlugins filters the plugin list using the predicates.
@@ -335,15 +336,29 @@ PluginLoop:
 // NetlogHandler, or PacketProcessorPlugin, their handler methods are
 // registered with the backend so they will receive these events.
 func (control *PluginControl) Startup() {
+	if control.wrapperInvokerFunc != nil && control.wrapper == nil {
+		if err := control.Invoke(control.wrapperInvokerFunc); err != nil {
+			panic(fmt.Sprintf("couldn't instantiate wrapper: %s", err))
+		}
+	}
 	control.filterPlugins()
-	for _, plugin := range control.pluginInfo {
-		if err := control.Invoke(plugin.saverFunc); err != nil {
-			panic(fmt.Sprintf("couldn't instantiate plugin: %s", err))
+	control.preparePlugins()
+	for _, pluginInf := range control.pluginInfo {
+		if err := control.Invoke(pluginInf.saverFunc); err != nil {
+			err = fmt.Errorf("couldn't instantiate plugin with constructor %T: %w", pluginInf.constructor, err)
+			if control.enableStartupPanic {
+				panic(err)
+			}
+			logger.Crit("%s\n", err.Error())
 		}
 	}
 
 	successfulPlugins := []*pluginInfo{}
 	for _, pluginInf := range control.pluginInfo {
+		if pluginInf.plugin == nil {
+			// Instantiation failed for this plugin, skip it.
+			continue
+		}
 		plugin := pluginInf.plugin
 		logger.Info("Starting plugin: %s\n", plugin.Name())
 		if err := plugin.Startup(); err != nil {
